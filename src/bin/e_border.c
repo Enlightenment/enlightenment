8e127335cbaa38c5753a701b16c02a69fa1bea61
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2013-01-09 13:24:24 +0000

    force window border (theme) inset to be applied to new client placement geometry, fixing issues where windows would appear to "slide" across the desktop by the size of the border inset; this was primarily noticeable when using wine applications
    SVN revision: 82460

--- src/bin/e_border.c
+++ src/bin/e_border.c
@@ -8570,13 +8570,18 @@ _e_border_eval(E_Border *bd)
                                  (abs((bd->zone->container->h / 2) - bd->y - (bd->h / 2)) < 3))) //bd->y - bd->h/2 is center of container
                                )
                               e_border_center(bd);
+                            else
+                              _e_border_move_internal(bd, bd->x, bd->y, 1);
                          }
+                       else
+                         _e_border_move_internal(bd, bd->x, bd->y, 1);
                        bd->changes.pos = 1;
                        bd->placed = 1;
                     }
                }
              else
                {
+                  _e_border_move_internal(bd, bd->x, bd->y, 1);
                   bd->changes.pos = 1;
                   bd->placed = 1;
                }
