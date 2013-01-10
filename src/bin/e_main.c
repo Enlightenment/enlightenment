8345d7d6d4a6bc53533324a4bef312e8964e4b4a
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2013-01-10 08:06:01 +0000

    order matters in e_main: e_comp would never succeed at compositing anything border-related because it was inited before e_border, thus causing all its e_border event handlers to be nonexistent
    SVN revision: 82520

--- src/bin/e_main.c
+++ src/bin/e_main.c
@@ -665,11 +665,6 @@ main(int argc, char **argv)
 
    e_screensaver_preinit();
 
-   TS("E_Comp Init");
-   e_comp_init();
-   TS("E_Comp Init Done");
-   _e_main_shutdown_push(e_comp_shutdown);
-
    if (e_config->show_splash)
      e_init_status_set(_("Setup Screens"));
    TS("Screens Init");
@@ -1665,6 +1660,12 @@ _e_main_screens_init(void)
         free(roots);
         return 0;
      }
+
+   TS("E_Comp Init");
+   e_comp_init();
+   TS("E_Comp Init Done");
+   _e_main_shutdown_push(e_comp_shutdown);
+
    TS("\tscreens: win");
    if (!e_win_init())
      {
