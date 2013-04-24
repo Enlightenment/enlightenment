09eb83798b3edaeab1cbbb66d874a679e1f46747
Chris Michael <cp.michael@samsung.com>
2013-04-24 13:55:06 +0100

    Add e_util_strcasecmp function prototype
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_wayland/e_utils.h
+++ src/bin/e_wayland/e_utils.h
@@ -5,6 +5,7 @@
 
 EAPI void e_util_env_set(const char *var, const char *val);
 EAPI int e_util_strcmp(const char *s1, const char *s2);
+EAPI int e_util_strcasecmp(const char *s1, const char *s2);
 
 # endif
 #endif
