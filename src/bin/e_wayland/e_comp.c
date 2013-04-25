c824e82451c2e9cf325c9861014a16c404befe81
Chris Michael <cp.michael@samsung.com>
2013-04-25 06:58:46 +0100

    Add code to initialize egl.
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_wayland/e_comp.c
+++ src/bin/e_wayland/e_comp.c
@@ -54,7 +54,44 @@ e_comp_init(void)
      ERR("Could not initialize SHM mechanism: %m");
 
 #ifdef HAVE_WAYLAND_EGL
-   /* try to initialize EGL */
+   /* try to get the egl display
+    * 
+    * NB: This is interesting....if we try to eglGetDisplay and pass in the 
+    * wayland display, then EGL fails due to XCB not owning the event queue.
+    * If we pass it a NULL, it inits just fine */
+   _e_comp->egl.display = eglGetDisplay(NULL);
+   if (_e_comp->egl.display == EGL_NO_DISPLAY)
+     ERR("Could not get EGL display: %m");
+   else
+     {
+        EGLint major, minor;
+
+        /* try to initialize EGL */
+        if (!eglInitialize(_e_comp->egl.display, &major, &minor))
+          {
+             ERR("Could not initialize EGL: %m");
+             eglTerminate(_e_comp->egl.display);
+          }
+        else
+          {
+             EGLint n;
+             EGLint attribs[] = 
+               {
+                  EGL_SURFACE_TYPE, EGL_WINDOW_BIT, 
+                  EGL_RED_SIZE, 1, EGL_GREEN_SIZE, 1, EGL_BLUE_SIZE, 1, 
+                  EGL_ALPHA_SIZE, 1, EGL_RENDERABLE_TYPE, 
+                  EGL_OPENGL_ES2_BIT, EGL_NONE
+               };
+
+             /* try to find a matching egl config */
+             if ((!eglChooseConfig(_e_comp->egl.display, attribs, 
+                                   &_e_comp->egl.config, 1, &n) || (n == 0)))
+               {
+                  ERR("Could not choose EGL config: %m");
+                  eglTerminate(_e_comp->egl.display);
+               }
+          }
+     }
 #endif
 
    return 1;
@@ -72,6 +109,11 @@ err:
 EINTERN int 
 e_comp_shutdown(void)
 {
+#ifdef HAVE_WAYLAND_EGL
+   /* destroy the egl display */
+   if (_e_comp->egl.display) eglTerminate(_e_comp->egl.display);
+#endif
+
    /* destroy the previously created display */
    if (_e_comp->wl.display) wl_display_destroy(_e_comp->wl.display);
 
