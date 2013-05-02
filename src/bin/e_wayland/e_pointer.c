73b12b9e33a7bffbab9c530dba0f4f316c2e2173
Chris Michael <cp.michael@samsung.com>
2013-05-02 09:33:29 +0100

    Use E_LIST_HANDLER_APPEND and E_FREE_LIST macros.
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_wayland/e_pointer.c
+++ src/bin/e_wayland/e_pointer.c
@@ -30,22 +30,14 @@ static Eina_List *_hdlrs = NULL;
 EINTERN int 
 e_pointer_init(void)
 {
-   _hdlrs = 
-     eina_list_append(_hdlrs, 
-                      ecore_event_handler_add(ECORE_EVENT_MOUSE_BUTTON_DOWN, 
-                                              _e_pointer_cb_mouse_down, NULL));
-   _hdlrs = 
-     eina_list_append(_hdlrs, 
-                      ecore_event_handler_add(ECORE_EVENT_MOUSE_BUTTON_UP, 
-                                              _e_pointer_cb_mouse_up, NULL));
-   _hdlrs = 
-     eina_list_append(_hdlrs, 
-                      ecore_event_handler_add(ECORE_EVENT_MOUSE_MOVE, 
-                                              _e_pointer_cb_mouse_move, NULL));
-   _hdlrs = 
-     eina_list_append(_hdlrs, 
-                      ecore_event_handler_add(ECORE_EVENT_MOUSE_WHEEL, 
-                                              _e_pointer_cb_mouse_wheel, NULL));
+   E_LIST_HANDLER_APPEND(_hdlrs, ECORE_EVENT_MOUSE_BUTTON_DOWN, 
+                         _e_pointer_cb_mouse_down, NULL));
+   E_LIST_HANDLER_APPEND(_hdlrs, ECORE_EVENT_MOUSE_BUTTON_UP, 
+                         _e_pointer_cb_mouse_up, NULL));
+   E_LIST_HANDLER_APPEND(_hdlrs, ECORE_EVENT_MOUSE_MOVE, 
+                         _e_pointer_cb_mouse_move, NULL));
+   E_LIST_HANDLER_APPEND(_hdlrs, ECORE_EVENT_MOUSE_WHEEL, 
+                         _e_pointer_cb_mouse_wheel, NULL));
 
    return 1;
 }
