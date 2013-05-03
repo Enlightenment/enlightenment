6d9b6dc97efc1bee58fb5bd2caf05f5972a48a08
Chris Michael <cp.michael@samsung.com>
2013-05-03 11:14:19 +0100

    Add shell_interface type.
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_wayland/e_shell.h
+++ src/bin/e_wayland/e_shell.h
@@ -1,7 +1,17 @@
 #ifdef E_TYPEDEFS
+
+typedef struct _E_Shell_Interface E_Shell_Interface;
+
 #else
 # ifndef E_SHELL_H
 #  define E_SHELL_H
+
+struct _E_Shell_Interface
+{
+   void *shell;
+
+   /* TODO: surface_create */
+};
 
 EINTERN int e_shell_init(void);
 EINTERN int e_shell_shutdown(void);
