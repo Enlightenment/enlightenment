59db3c7b8916f951c434edd5533566b550b09f66
Chris Michael <cp.michael@samsung.com>
2013-05-03 11:26:13 +0100

    Add code to load the shell module.
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_wayland/e_shell.c
+++ src/bin/e_wayland/e_shell.c
@@ -3,11 +3,40 @@
 EINTERN int 
 e_shell_init(void)
 {
-   return 1;
+   E_Module *mod = NULL;
+   const char *modname;
+
+   /* NB: Basically, this function needs to load the appropriate 
+    * shell module (desktop, tablet, etc) */
+
+   /* FIXME: Get this from config */
+   modname = "wl_desktop";
+
+   if (!(mod = e_module_find(modname)))
+     mod = e_module_new(modname);
+
+   if (mod) 
+     {
+        if ((e_module_enable(mod)))
+          return 1;
+     }
+
+   return 0;
 }
 
 EINTERN int 
 e_shell_shutdown(void)
 {
+   E_Module *mod = NULL;
+   const char *modname;
+
+   /* NB: This function needs to unload the shell module */
+
+   /* FIXME: Get this from config */
+   modname = "wl_desktop";
+
+   if ((mod = e_module_find(modname)))
+     e_module_disable(mod);
+
    return 1;
 }
