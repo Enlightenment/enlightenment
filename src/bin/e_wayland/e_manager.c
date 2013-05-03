a775780c6be3a85bd8132e12174d8b02d1f3dc1c
Chris Michael <cp.michael@samsung.com>
2013-05-03 07:01:32 +0100

    Add code to create new manager objects.
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_wayland/e_manager.c
+++ src/bin/e_wayland/e_manager.c
@@ -1,6 +1,7 @@
 #include "e.h"
 
 /* local function prototypes */
+static void _e_manager_cb_free(E_Manager *man);
 
 /* local variables */
 static Eina_List *_managers = NULL;
@@ -23,4 +24,37 @@ EAPI Eina_List *
 e_manager_list(void)
 {
    return _managers;
+}
+
+EAPI E_Manager *
+e_manager_new(E_Output *output, int num)
+{
+   E_Manager *man;
+
+   /* try to create new manager object */
+   if (!(man = E_OBJECT_ALLOC(E_Manager, E_MANAGER_TYPE, _e_manager_cb_free)))
+     return NULL;
+
+   /* set manager properties */
+   man->num = num;
+   man->x = output->x;
+   man->y = output->y;
+   man->w = output->w;
+   man->h = output->h;
+
+   /* add this manager to the list */
+   _managers = eina_list_append(_managers, man);
+
+   return man;
+}
+
+/* local functions */
+static void 
+_e_manager_cb_free(E_Manager *man)
+{
+   /* remove this manager from the list */
+   _managers = eina_list_remove(_managers, man);
+
+   /* free the object */
+   E_FREE(man);
 }
