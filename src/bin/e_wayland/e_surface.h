cfe61021825a4da5f83c50a00366d067fdd5ac0f
Chris Michael <cp.michael@samsung.com>
2013-05-03 12:44:30 +0100

    Add a reference to the shell surface Add map/unmap/configure function pointers to structure.
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_wayland/e_surface.h
+++ src/bin/e_wayland/e_surface.h
@@ -13,6 +13,14 @@ struct _E_Surface
         struct wl_surface surface;
         struct wl_list link;
      } wl;
+
+   E_Shell_Surface *shell_surface;
+
+   Eina_Bool mapped : 1;
+
+   void (*map)(E_Surface *surface, Evas_Coord x, Evas_Coord y, Evas_Coord w, Evas_Coord h);
+   void (*unmap)(E_Surface *surface);
+   void (*configure)(E_Surface *surface, Evas_Coord x, Evas_Coord y, Evas_Coord w, Evas_Coord h);
 };
 
 EAPI E_Surface *e_surface_new(unsigned int id);
