bd97b2c4428b2fcee21b0e77f8e9c705511a41a8
Chris Michael <cp.michael@samsung.com>
2013-05-07 13:34:59 +0100

    Change e_plane to use pixman regions.
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_wayland/e_plane.c
+++ src/bin/e_wayland/e_plane.c
@@ -5,13 +5,13 @@ e_plane_init(E_Plane *plane, Evas_Coord x, Evas_Coord y)
 {
    plane->x = x;
    plane->y = y;
-   plane->damage = eina_rectangle_new(x, y, 0, 0);
-   plane->clip = eina_rectangle_new(x, y, 0, 0);
+   pixman_region32_init(&plane->damage);
+   pixman_region32_init(&plane->clip);
 }
 
 EAPI void 
 e_plane_shutdown(E_Plane *plane)
 {
-   eina_rectangle_free(plane->damage);
-   eina_rectangle_free(plane->clip);
+   pixman_region32_fini(&plane->damage);
+   pixman_region32_fini(&plane->clip);
 }
