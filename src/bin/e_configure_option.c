c1f51a82a65b415e079993233afae2b1e264009b
Igor Murzov <e-mail@date.by>
2013-06-09 18:57:55 +0400

    Add missing space to the message

--- src/bin/e_configure_option.c
+++ src/bin/e_configure_option.c
@@ -1603,7 +1603,7 @@ e_configure_option_init(void)
    OPT_MINMAX_STEP_FMT(0, 100, 2, _("%2.0f pixels"));
 
    OPT_ADD(BOOL, geometry_auto_move, _("Ensure initial placement of windows inside useful geometry"), _("border"), _("placement"));
-   OPT_HELP(_("Useful geometry is calculated as the screen size minus the geometry of any shelves which do not allow windows to overlap them."
+   OPT_HELP(_("Useful geometry is calculated as the screen size minus the geometry of any shelves which do not allow windows to overlap them. "
               "This option ensures that non-user placement of windows will be inside the useful geometry of the current screen"));
    OPT_ADD(BOOL, geometry_auto_resize_limit, _("Limit window autoresizing to useful geometry"), _("border"), _("placement"), _("resize"));
    OPT_HELP(_("Useful geometry is calculated as the screen size minus the geometry of any shelves which do not allow windows to overlap them"));
