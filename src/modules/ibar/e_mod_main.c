84dc539ddf32180d8efb16eb6e84a51002d8aed8
Mike Blumenkrantz <m.blumenkran@samsung.com>
2013-08-13 11:04:55 +0100

    ibar obeys max size hint

--- src/modules/ibar/e_mod_main.c
+++ src/modules/ibar/e_mod_main.c
@@ -590,6 +590,13 @@ _ibar_resize_handle(IBar *b)
    Evas_Coord w, h;
 
    evas_object_geometry_get(b->o_box, NULL, NULL, &w, &h);
+   if (b->inst->gcc)
+     {
+        if (b->inst->gcc->max.w)
+          w = MIN(w, b->inst->gcc->max.w);
+        if (b->inst->gcc->max.h)
+          h = MIN(h, b->inst->gcc->max.h);
+     }
    if (e_box_orientation_get(b->o_box))
      w = h;
    else
