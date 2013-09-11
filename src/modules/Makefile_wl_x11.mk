87b16dad61fd137b537f8639671d095faf69821f
Mike Blumenkrantz <zmike@samsung.com>
2013-09-11 14:32:00 +0100

    feature: add wl_x11 output module
    this is the first of the wayland output modules, and it is extremely simple as a result of the compositor rewrite. for any other wayland output modules, it should be enough to simply create an ecore_evas canvas and do startup in this way.

---
+++ src/modules/Makefile_wl_x11.mk
@@ -0,0 +1,15 @@
+wl_x11dir = $(MDIR)/wl_x11
+
+wl_x11pkgdir = $(MDIR)/wl_x11/$(MODULE_ARCH)
+wl_x11pkg_LTLIBRARIES = src/modules/wl_x11/module.la
+
+src_modules_wl_x11_module_la_DEPENDENCIES = $(MDEPENDENCIES)
+src_modules_wl_x11_module_la_CPPFLAGS  = $(MOD_CPPFLAGS) @ECORE_X_CFLAGS@ @WAYLAND_CFLAGS@ -DNEED_X=1
+src_modules_wl_x11_module_la_LIBADD   = $(LIBS) @ECORE_X_LIBS@ @WAYLAND_LIBS@
+src_modules_wl_x11_module_la_LDFLAGS = $(MOD_LDFLAGS)
+src_modules_wl_x11_module_la_SOURCES = src/modules/wl_x11/e_mod_main.c
+
+# TODO: incomplete
+#.PHONY: wl_x11 install-wl_x11
+#wl_x11: $(wl_x11pkg_LTLIBRARIES) $(wl_x11_DATA)
+#install-wl_x11: install-wl_x11DATA install-wl_x11pkgLTLIBRARIES
