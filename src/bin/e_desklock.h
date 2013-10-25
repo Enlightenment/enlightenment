a31e8a70fb367e0ccb8f4eb7215902eb73433766
discomfitor <michael.blumenkrantz@gmail.com>
2013-10-25 14:20:45 +0100

    break out desklock into display server-able hooks
    if we're running in a non-X environment, we can't very well expect that using X calls to hide/show windows for desklock is going to work as expected. now a compositor backend can add a pre or post desklock hook to hide/show its clients as necessary

--- src/bin/e_desklock.h
+++ src/bin/e_desklock.h
@@ -1,6 +1,8 @@
 #ifdef E_TYPEDEFS
 
 typedef struct _E_Event_Desklock E_Event_Desklock;
+typedef Eina_Bool (*E_Desklock_Show_Cb)(void);
+typedef void (*E_Desklock_Hide_Cb)(void);
 
 typedef enum _E_Desklock_Background_Method {
     E_DESKLOCK_BACKGROUND_METHOD_THEME_DESKLOCK = 0,
@@ -25,6 +27,11 @@ EAPI int e_desklock_show(Eina_Bool suspend);
 EAPI int e_desklock_show_autolocked(void);
 EAPI void e_desklock_hide(void);
 EAPI Eina_Bool e_desklock_state_get(void);
+
+EAPI void e_desklock_show_hook_add(E_Desklock_Show_Cb cb);
+EAPI void e_desklock_show_hook_del(E_Desklock_Show_Cb cb);
+EAPI void e_desklock_hide_hook_add(E_Desklock_Hide_Cb cb);
+EAPI void e_desklock_hide_hook_del(E_Desklock_Hide_Cb cb);
 
 extern EAPI int E_EVENT_DESKLOCK;
 
