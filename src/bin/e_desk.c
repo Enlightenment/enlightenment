8ad5e3db391df1cc98051d2f94877245a0b2ca27
Mike Blumenkrantz <zmike@samsung.com>
2013-11-26 12:43:15 -0500

    ignore shelves from other screens when changing vdesks
    T485

--- src/bin/e_desk.c
+++ src/bin/e_desk.c
@@ -309,6 +309,7 @@ e_desk_show(E_Desk *desk)
 
    EINA_LIST_FOREACH(e_shelf_list(), l, es)
      {
+        if (es->zone != desk->zone) continue;
         if (e_shelf_desk_visible(es, desk))
           e_shelf_show(es);
         else
