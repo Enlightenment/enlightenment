fcc82e3e342ca639f849669cec77127bd4ff7a23
Sebastian Dransfeld <sd@tango.flipp.net>
2013-12-07 22:09:49 +0100

    Fix 03d50e9546ced3f7c135cad6617f4e397a024fce
    If this ever worked it's a miracle. remember_sigill is initialized for
    each loop, so it can't remember anything.
    
    Fixes CID 1039950

--- src/bin/e_start_main.c
+++ src/bin/e_start_main.c
@@ -490,6 +490,8 @@ main(int argc, char **argv)
              pid_t result;
              int status;
              Eina_Bool done = EINA_FALSE;
+             Eina_Bool remember_sigill = EINA_FALSE;
+             Eina_Bool remember_sigusr1 = EINA_FALSE;
 
 #ifdef HAVE_SYS_PTRACE_H
              if (!really_know)
@@ -503,9 +505,6 @@ main(int argc, char **argv)
 #endif
              while (!done)
                {
-                  Eina_Bool remember_sigill = EINA_FALSE;
-                  Eina_Bool remember_sigusr1 = EINA_FALSE;
-
                   result = waitpid(child, &status, WNOHANG);
                   if (!result)
                     {
