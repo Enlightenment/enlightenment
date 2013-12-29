ab268a42b017b798e7a98e79d6ac632091713469
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2013-12-29 15:08:39 +0900

    evrything - fixes strict aliasing wanring and was a stupid cast anyway
    this fixes strict aliasing wanrings (-Wstrict-aliasing) whihc are a
    result of imported md5 code being just really stupid with casting an
    array to a ptr when there is no need.

--- src/modules/everything/md5.c
+++ src/modules/everything/md5.c
@@ -150,8 +150,8 @@ MD5Final(unsigned char digest[16], MD5_CTX *ctx)
    byteReverse(ctx->in, 14);
 
    /* Append length in bits and transform */
-   ((uint32_t *)ctx->in)[14] = ctx->bits[0];
-   ((uint32_t *)ctx->in)[15] = ctx->bits[1];
+   ctx->in[14] = ctx->bits[0];
+   ctx->in[15] = ctx->bits[1];
 
    MD5Transform(ctx->buf, (uint32_t *)ctx->in);
    byteReverse((unsigned char *)ctx->buf, 4);
