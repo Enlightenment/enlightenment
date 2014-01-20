d0713cddb4dbb2443791ed0338cd600768edfc27
Mike Blumenkrantz <zmike@samsung.com>
2014-01-20 17:27:31 -0500

    fix shading animations
    yeeeep it's that simple. should probably move these to edje at some point

--- src/bin/e_comp_object.c
+++ src/bin/e_comp_object.c
@@ -1946,9 +1946,7 @@ _e_comp_smart_resize(Evas_Object *obj, int w, int h)
      }
    else
      {
-        e_comp_object_frame_wh_adjust(obj, cw->ec->client.w, cw->ec->client.h, &w, &h);
         evas_object_resize(cw->effect_obj, w, h);
-        if (cw->zoomobj) e_zoomap_child_resize(cw->zoomobj, cw->ec->client.w, cw->ec->client.h);
      }
    e_comp_shape_queue(cw->comp);
 }
