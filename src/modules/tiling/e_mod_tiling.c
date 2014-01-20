ca6f1e74f5db7c9f216bef3ecf9585e8156fc226
Tom Hacohen <tom@stosb.com>
2014-01-20 16:13:20 +0000

    Tiling2: Fixed removing clients from extras hash.

--- src/modules/tiling/e_mod_tiling.c
+++ src/modules/tiling/e_mod_tiling.c
@@ -510,7 +510,7 @@ _remove_client(E_Client *ec)
          _G.tinfo->tree = tiling_window_tree_remove(_G.tinfo->tree, item);
       }
 
-    eina_hash_del(_G.client_extras, ec, NULL);
+    eina_hash_del(_G.client_extras, &ec, NULL);
 
     _reapply_tree();
 }
