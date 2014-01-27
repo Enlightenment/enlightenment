f54d669f43003b8dddd80a305d1619218c18c045
Mike Blumenkrantz <zmike@samsung.com>
2014-01-27 16:22:57 -0500

    add forgotten proto for e_widget_filepreview_clamp_video_set

--- src/bin/e_widget_filepreview.h
+++ src/bin/e_widget_filepreview.h
@@ -6,6 +6,7 @@
 EAPI Evas_Object *e_widget_filepreview_add(Evas *evas, int w, int h, int horiz);
 EAPI void e_widget_filepreview_path_set(Evas_Object *obj, const char *path, const char *mime);
 EAPI void e_widget_filepreview_filemode_force(Evas_Object *obj);
+EAPI void e_widget_filepreview_clamp_video_set(Evas_Object *obj, Eina_Bool clamp);
 
 #endif
 #endif
