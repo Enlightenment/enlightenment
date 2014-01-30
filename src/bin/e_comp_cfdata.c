2de63a2f0b404e1a34ce299fc89552affe023bd1
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2014-01-31 08:50:37 +0900

    comp matches - add menu and dnd types in list

--- src/bin/e_comp_cfdata.c
+++ src/bin/e_comp_cfdata.c
@@ -170,6 +170,16 @@ e_comp_cfdata_config_new(void)
 
    mat = E_NEW(E_Comp_Match, 1);
    cfg->match.overrides = eina_list_append(cfg->match.overrides, mat);
+   mat->primary_type = E_WINDOW_TYPE_MENU;
+   mat->shadow_style = eina_stringshare_add("menu");
+
+   mat = E_NEW(E_Comp_Match, 1);
+   cfg->match.overrides = eina_list_append(cfg->match.overrides, mat);
+   mat->primary_type = E_WINDOW_TYPE_DND;
+   mat->shadow_style = eina_stringshare_add("still");
+
+   mat = E_NEW(E_Comp_Match, 1);
+   cfg->match.overrides = eina_list_append(cfg->match.overrides, mat);
    mat->name = eina_stringshare_add("geany");
    mat->clas = eina_stringshare_add("Geany");
    mat->primary_type = E_WINDOW_TYPE_NORMAL;
