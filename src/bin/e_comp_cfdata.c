9d9ee6c33b4025fded7ac84ddab71d45b28c706f
Mike Blumenkrantz <zmike@samsung.com>
2014-02-14 12:46:53 -0500

    add default comp match for DOCK window types

--- src/bin/e_comp_cfdata.c
+++ src/bin/e_comp_cfdata.c
@@ -180,6 +180,11 @@ e_comp_cfdata_config_new(void)
 
    mat = E_NEW(E_Comp_Match, 1);
    cfg->match.overrides = eina_list_append(cfg->match.overrides, mat);
+   mat->primary_type = E_WINDOW_TYPE_DOCK;
+   mat->shadow_style = eina_stringshare_add("still");
+
+   mat = E_NEW(E_Comp_Match, 1);
+   cfg->match.overrides = eina_list_append(cfg->match.overrides, mat);
    mat->shadow_style = eina_stringshare_add("popup");
 
    cfg->match.menus = NULL;
