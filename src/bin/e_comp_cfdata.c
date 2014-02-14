92c7ec1dadb892a34cbd10f745ae8cfa49cc11e2
Mike Blumenkrantz <zmike@samsung.com>
2014-02-14 13:05:00 -0500

    DOCKs should actually be "none" so they don't get shadows

--- src/bin/e_comp_cfdata.c
+++ src/bin/e_comp_cfdata.c
@@ -181,7 +181,7 @@ e_comp_cfdata_config_new(void)
    mat = E_NEW(E_Comp_Match, 1);
    cfg->match.overrides = eina_list_append(cfg->match.overrides, mat);
    mat->primary_type = E_WINDOW_TYPE_DOCK;
-   mat->shadow_style = eina_stringshare_add("still");
+   mat->shadow_style = eina_stringshare_add("none");
 
    mat = E_NEW(E_Comp_Match, 1);
    cfg->match.overrides = eina_list_append(cfg->match.overrides, mat);
