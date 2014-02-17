1bbcf1573a6024ae9882c2bff73afb6ea1ff2716
Mike Blumenkrantz <zmike@samsung.com>
2014-02-17 12:02:27 -0500

    bugfix: unref client when removing from exec if exec is deleted
    fixes a race condition where client would never be freed when closing lots of windows simultaneously

--- src/bin/e_border.c
+++ src/bin/e_border.c
@@ -4913,7 +4913,11 @@ _e_border_del(E_Border *bd)
         if (bd->exe_inst->phony && (eina_list_count(bd->exe_inst->borders) == 1))
           e_exec_phony_del(bd->exe_inst);
         else
-          bd->exe_inst->borders = eina_list_remove(bd->exe_inst->borders, bd);
+          {
+             bd->exe_inst->borders = eina_list_remove(bd->exe_inst->borders, bd);
+             if (bd->exe_inst->deleted)
+               e_object_unref(E_OBJECT(bd));
+          }
         bd->exe_inst = NULL;
      }
 
