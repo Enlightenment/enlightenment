ce7da97d8e8f544129bde37ae0540f9ea9e1f300
Tom Hacohen <tom@stosb.com>
2014-02-18 16:33:14 +0000

    Tiling2: Ifdef out ecore_x specific stuff when in Wayland Only.

--- src/modules/tiling/e_mod_tiling.c
+++ src/modules/tiling/e_mod_tiling.c
@@ -164,8 +164,10 @@ is_tilable(const E_Client *ec)
    if (ec->icccm.min_h == ec->icccm.max_h && ec->icccm.max_h > 0)
      return false;
 
+#ifndef WAYLAND_ONLY
    if (ec->icccm.gravity == ECORE_X_GRAVITY_STATIC)
      return false;
+#endif
 
    if (ec->e.state.centered)
      return false;
