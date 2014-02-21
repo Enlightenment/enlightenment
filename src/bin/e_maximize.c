653f3499c01eda9a8f890ea3947efccfb5ed9689
Mike Blumenkrantz <zmike@samsung.com>
2014-02-21 16:35:04 -0500

    fix e_maximize_client_shelf_fill() to account for all shelves

--- src/bin/e_maximize.c
+++ src/bin/e_maximize.c
@@ -128,7 +128,8 @@ e_maximize_border_shelf_fill(E_Border *bd, int *x1, int *yy1, int *x2, int *y2, 
    E_Shelf *es;
    E_Maximize_Rect *r;
 
-   EINA_LIST_FOREACH(e_shelf_list(), l, es)
+   l = e_shelf_list_all();
+   EINA_LIST_FREE(l, es)
      {
 	Eina_List *ll;
 	E_Config_Shelf_Desk *sd;
