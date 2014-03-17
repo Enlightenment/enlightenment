a2025ddaadf234e6747406ce461bde04a5e104b9
Mike Blumenkrantz <zmike@samsung.com>
2014-03-17 12:56:21 -0400

    ignore size change configurerequests during maximize if not also changing position
    correcty fixes gtk window (un)maximize

--- src/bin/e_comp_x.c
+++ src/bin/e_comp_x.c
@@ -1501,7 +1501,7 @@ _e_comp_x_configure_request(void *data  EINA_UNUSED, int type EINA_UNUSED, Ecore
           }
      }
 
-   if (resize && (!ec->lock_client_size))
+   if (resize && (!ec->lock_client_size) && (move || (!ec->maximized)))
      {
         if ((ec->maximized & E_MAXIMIZE_TYPE) != E_MAXIMIZE_NONE)
           {
