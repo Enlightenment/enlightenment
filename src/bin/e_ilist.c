a08c40df7b14893cbf6e91b18ce4297727109cd1
Sebastian Dransfeld <sd@tango.flipp.net>
2014-03-24 13:26:20 +0100

    ilist: Remove unused assignment
    CID 1039758

--- src/bin/e_ilist.c
+++ src/bin/e_ilist.c
@@ -466,7 +466,6 @@ e_ilist_remove_num(Evas_Object *obj, int n)
     */
    if (!resize) return;
    if (!sd->items) return;
-   si = eina_list_data_get(sd->items);
    evas_object_resize(sd->o_smart, w, sd->h - h);
 }
 
