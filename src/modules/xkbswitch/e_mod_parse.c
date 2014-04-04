19bc0d5a5e2b9ef9d13508ef854b5450520bc322
Mike Blumenkrantz <zmike@samsung.com>
2014-04-04 15:14:08 -0400

    reduce strstr use in xkb parser
    CID 1039849

--- src/modules/xkbswitch/e_mod_parse.c
+++ src/modules/xkbswitch/e_mod_parse.c
@@ -236,9 +236,9 @@ parse_rules(void)
                      /* A hack to get it to parse right if
                       * the group name contains a space
                       */
-                     if (strstr(p, "  "))
+                     p = strstr(p, "  ");
+                     if (p)
                        {
-                          p = strstr(p, "  ");
                           while (p[0] == ' ')
                             ++p;
                        }
