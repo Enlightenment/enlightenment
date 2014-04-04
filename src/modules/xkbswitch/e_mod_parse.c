cb3548f0f6be04ebe587d76332a931d90c59a6e5
Mike Blumenkrantz <zmike@samsung.com>
2014-04-04 13:25:05 -0400

    null check xkb parser
    CID 1193251

--- src/modules/xkbswitch/e_mod_parse.c
+++ src/modules/xkbswitch/e_mod_parse.c
@@ -166,7 +166,7 @@ parse_rules(void)
      {
         if (fgets(buf, sizeof(buf), f))
           {
-             char *n, *p, *tmp, *tok, *txt;
+             char *n, *p, *tmp, *tok, *txt, *c;
 
              n = strchr(buf, '\n');
              if (n) *n = '\0';
@@ -180,7 +180,9 @@ parse_rules(void)
              variant->name = eina_stringshare_add(strtok(tmp, " "));
 
              tok = strtok(NULL, " ");
-             *strchr(tok, ':') = '\0';
+             c = strchr(tok, ':');
+             if (c)
+               *c = '\0';
 
              layout = eina_list_search_unsorted(layouts, layout_sort_by_name_cb, tok);
              layout->variants = eina_list_append(layout->variants, variant);
