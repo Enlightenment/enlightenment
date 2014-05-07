3679550b5c06471b2801f5313625938c9ee565e2
Jérémy Zurcher <jeremy@asynk.ch>
2014-05-07 12:16:10 +0200

    e_fm: add missing EINA_UNUSED

--- src/bin/e_fm.c
+++ src/bin/e_fm.c
@@ -11470,7 +11470,8 @@ e_fm2_operation_abort(int id)
 }
 
 EAPI Eina_Bool
-e_fm2_optimal_size_calc(Evas_Object *obj, int minw, int minh, int maxw, int maxh, int *w, int *h)
+e_fm2_optimal_size_calc(Evas_Object *obj, int minw, int minh EINA_UNUSED,
+                        int maxw, int maxh, int *w, int *h)
 {
    int x, y, step_w, step_h;
    EFM_SMART_CHECK(EINA_FALSE);
