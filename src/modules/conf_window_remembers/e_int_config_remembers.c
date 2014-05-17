e14b28a18b4c0f5c6c3ede1a20abdd5b9ed73f4e
Chris Michael <devilhorns@comcast.net>
2014-05-17 17:52:54 +0100

    add missing EINA_UNUSED for unused param
    Signed-off-by: Chris Michael <devilhorns@comcast.net>

--- src/modules/conf_window_remembers/e_int_config_remembers.c
+++ src/modules/conf_window_remembers/e_int_config_remembers.c
@@ -118,7 +118,7 @@ _basic_apply_data(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata)
 }
 
 static Evas_Object *
-_basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
+_basic_create(E_Config_Dialog *cfd EINA_UNUSED, Evas *evas, E_Config_Dialog_Data *cfdata)
 {
    Evas_Object *ol, *of2, *ow, *oc;
    Evas_Coord mw, mh;
