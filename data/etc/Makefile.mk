3b899f44fe7dc3aba186a53d46f5a58255d1341f
Sebastian Dransfeld <sd@tango.flipp.net>
2014-06-11 10:43:26 +0200

    e: rename applications menu to fit xdg menu spec

--- data/etc/Makefile.mk
+++ data/etc/Makefile.mk
@@ -9,7 +9,7 @@ etcmenusdir = $(sysconfdir)/xdg/menus
 etcmenus_DATA =
 
 if INSTALL_ENLIGHTENMENT_MENU
-etcmenus_DATA += data/etc/enlightenment.menu
+etcmenus_DATA += data/etc/e-applications.menu
 endif
 
 EXTRA_DIST += $(etcfiles_DATA) $(etcmenus_DATA)
