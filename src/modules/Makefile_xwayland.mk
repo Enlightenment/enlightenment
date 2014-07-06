3af3abf4525b743b62c6f6b91aac33b50592a686
Chris Michael <cp.michael@samsung.com>
2014-07-06 10:11:38 -0400

    xwayland: Fix makefile to use xwayland binary path
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/modules/Makefile_xwayland.mk
+++ src/modules/Makefile_xwayland.mk
@@ -10,7 +10,10 @@ xwaylandpkgdir = $(MDIR)/xwayland/$(MODULE_ARCH)
 xwaylandpkg_LTLIBRARIES = src/modules/xwayland/module.la
 
 src_modules_xwayland_module_la_DEPENDENCIES = $(MDEPENDENCIES)
-src_modules_xwayland_module_la_CPPFLAGS  = $(MOD_CPPFLAGS) @XWAYLAND_CFLAGS@ @WAYLAND_CFLAGS@
+src_modules_xwayland_module_la_CPPFLAGS  = \
+  $(MOD_CPPFLAGS) @XWAYLAND_CFLAGS@ @WAYLAND_CFLAGS@ \
+  -DXWAYLAND_BIN=\"@XWAYLAND_BIN@\"
+
 src_modules_xwayland_module_la_LIBADD   = $(LIBS) @XWAYLAND_LIBS@ @WAYLAND_LIBS@
 src_modules_xwayland_module_la_LDFLAGS = $(MOD_LDFLAGS)
 
