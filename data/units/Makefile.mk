28ac706fb0a61adeefa56db22046a3145b11f8da
Jérémy Zurcher <jeremy@asynk.ch>
2014-07-29 10:15:17 +0200

    e18.service -> enlightenment.service : Part II

--- data/units/Makefile.mk
+++ data/units/Makefile.mk
@@ -1,6 +1,6 @@
 if HAVE_SYSTEMD_USER_SESSION
 unitsdir = $(USER_SESSION_DIR)
-units_DATA = data/units/e18.service
+units_DATA = data/units/enlightenment.service
 endif
 
 EXTRA_DIST += $(units_DATA)
