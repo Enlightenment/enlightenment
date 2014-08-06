0bb9acc75d060f0186ce31d232e1baf9e65f14ec
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2014-08-06 12:04:27 +0900

    comp - with sw comp tell apps to also use sw - disable accel requests
    this addresses a vbox gl asccel issue where gl client apps can cause
    al of vbox to crash and users find it unexpected that e is set to
    software compositing but apps go and use opengl, so it's more
    consistent for users

--- src/bin/e_comp_x.c
+++ src/bin/e_comp_x.c
@@ -5008,6 +5008,14 @@ _e_comp_x_setup(E_Comp *c, Ecore_X_Window root, int w, int h)
         if (e_comp_config_get()->engine == E_COMP_ENGINE_GL)
           ecore_job_add(_e_comp_x_add_fail_job, NULL);
         e_comp_gl_set(0);
+        // tell elm and all elm apps to not allow acceleration since comp
+        // can't do it (or doesn't want to), so this may cause issues in
+        // gl drivers etc. - this addresses a vbox crash bug with vm
+        // opengl acceleration
+        elm_config_preferred_engine_set("none");
+        elm_config_accel_preference_override_set(EINA_TRUE);
+        elm_config_all_flush();
+        elm_config_save();
      }
 
    ecore_x_composite_redirect_subwindows(root, ECORE_X_COMPOSITE_UPDATE_MANUAL);
