92514408672e865b1ca8a16c9ea53952ca266224
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2014-08-19 18:44:47 +0900

    fix evrything shift+tab crash
    fixes T1548

--- src/modules/everything/evry_plug_apps.c
+++ src/modules/everything/evry_plug_apps.c
@@ -811,8 +811,9 @@ _complete(Evry_Plugin *plugin __UNUSED__, const Evry_Item *it, char **input)
    GET_APP(app, it);
 
    char buf[128];
+   if (it->subtype != EVRY_TYPE_APP) return 0;
 
-   if (app->desktop)
+   if ((app->desktop) && (app->desktop->exec))
      {
         char *space = strchr(app->desktop->exec, ' ');
 
