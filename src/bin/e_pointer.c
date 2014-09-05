bb2dab1a5d6d83ee0c3c9a68de35c740aa9205d9
Chris Michael <cp.michael@samsung.com>
2014-09-05 11:40:00 -0400

    bugfix: Set pointer timer & poller members to NULL after we delete the timer/poller
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_pointer.c
+++ src/bin/e_pointer.c
@@ -130,6 +130,8 @@ _e_pointer_active_handle(E_Pointer *ptr)
      {
         E_FREE_FUNC(ptr->idle_tmr, ecore_timer_del);
         E_FREE_FUNC(ptr->idle_poll, ecore_poller_del);
+        ptr->idle_tmr = NULL;
+        ptr->idle_poll = NULL;
         if (e_powersave_mode_get() >= E_POWERSAVE_MODE_MEDIUM) return;
         if (!e_config->idle_cursor) return;
         ptr->idle_tmr = ecore_timer_loop_add(1.0, _e_pointer_cb_idle_pre, ptr);
@@ -345,6 +347,8 @@ _e_pointer_cb_free(E_Pointer *ptr)
 
    E_FREE_FUNC(ptr->idle_tmr, ecore_timer_del);
    E_FREE_FUNC(ptr->idle_poll, ecore_poller_del);
+   ptr->idle_tmr = NULL;
+   ptr->idle_poll = NULL;
 
    if (!ptr->canvas) _e_pointer_canvas_del(ptr);
 
