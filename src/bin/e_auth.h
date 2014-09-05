721dfe82c2edeb42db01fddd0f6f500e59eb7a7a
Mike Blumenkrantz <zmike@osg.samsung.com>
2014-09-05 15:02:10 -0400

    add e_auth_hash_djb2 inline
    all eina hash functions are now useless for any case where the hashed value needs to be stored, so it's necessary for e to provide its own hash function to use for password storage

--- src/bin/e_auth.h
+++ src/bin/e_auth.h
@@ -4,4 +4,17 @@
 EAPI int e_auth_begin(char *passwd);
 EAPI char *e_auth_hostname_get(void);
 
+static inline int
+e_auth_hash_djb2(const char *key, int len)
+{
+   unsigned int hash_num = 5381;
+   const unsigned char *ptr;
+
+   if (!key) return 0;
+   for (ptr = (unsigned char *)key; len; ptr++, len--)
+     hash_num = ((hash_num << 5) + hash_num) ^ *ptr; /* hash * 33 ^ c */
+
+   return (int)hash_num;
+}
+
 #endif
