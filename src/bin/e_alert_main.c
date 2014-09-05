c281d6da82bc2ef9d380a7eef2a148c19558d209
Stefan Schmidt <s.schmidt@samsung.com>
2014-09-05 12:12:13 +0200

    e_alert_main: Don't monitor in E in wayland only mode to avoid endless restarts
    When running in Wayland only mode we don't have XCB available so a
    xcb_connection would fail and we would restart endlessly.
    
    I have been bitten by that often enough now. Better shutdown E cleanly and don't
    use the watchdog functionality for now. This needs a wayland specific solution
    anyway.

--- src/bin/e_alert_main.c
+++ src/bin/e_alert_main.c
@@ -66,6 +66,12 @@ main(int argc, char **argv)
    const char *tmp;
    int i = 0;
 
+/* XCB is not avaibale when running in wayland only mode. No need to start anything here */
+#ifdef HAVE_WAYLAND_ONLY
+   printf("E Alert is not suitable to be used with E in wayland only mode\n");
+   exit(0);
+#endif
+
    for (i = 1; i < argc; i++)
      {
         if ((!strcmp(argv[i], "-h")) ||
