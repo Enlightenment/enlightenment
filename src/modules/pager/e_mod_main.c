c70c33479224b67a14b8efef15f9ab56d8860713
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2014-09-11 16:24:54 +0900

    Revert "redo pager modules to use more accurately named edje groups"
    This reverts commit 9488fddc59f4588984fa466b547b0522831da07e.
    
    no - this breaks theme api. AGAIN ... like e18. see email to e-devel.

--- src/modules/pager/e_mod_main.c
+++ src/modules/pager/e_mod_main.c
@@ -466,7 +466,7 @@ _pager_desk_new(Pager *p, E_Desk *desk, int xpos, int ypos, Eina_Bool invert)
    o = edje_object_add(e);
    pd->o_desk = o;
    e_theme_edje_object_set(o, "base/theme/modules/pager",
-                           "e/modules/pager/desk");
+                           "e/modules/pager16/desk");
    edje_object_part_text_set(o, "e.text.label", desk->name);
    if (pager_config->show_desk_names)
      edje_object_signal_emit(o, "e,name,show", "e");
@@ -690,7 +690,7 @@ _pager_window_new(Pager_Desk *pd, Evas_Object *mirror, E_Client *client)
    //o = edje_object_add(evas_object_evas_get(pd->pager->o_table));
    //pw->o_window = o;
    //e_theme_edje_object_set(o, "base/theme/modules/pager",
-                           //"e/modules/pager/window");
+                           //"e/modules/pager16/window");
    //if (visible) evas_object_show(o);
 
 
@@ -783,7 +783,7 @@ _pager_popup_new(E_Zone *zone, int keyaction)
    pp->o_bg = edje_object_add(zone->comp->evas);
    evas_object_name_set(pp->o_bg, "pager_popup");
    e_theme_edje_object_set(pp->o_bg, "base/theme/modules/pager",
-                           "e/modules/pager/popup");
+                           "e/modules/pager16/popup");
    desk = e_desk_current_get(zone);
    if (desk)
      edje_object_part_text_set(pp->o_bg, "e.text.label", desk->name);
