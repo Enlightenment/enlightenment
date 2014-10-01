702e32ea2a09210259a7438dad071a1e43e86a77
Chris Michael <cp.michael@samsung.com>
2014-10-01 16:14:05 -0400

    wl-desktop-shell: Update desktop shell module for new xdg shell protocol
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/modules/wl_desktop_shell/e_desktop_shell_protocol.h
+++ src/modules/wl_desktop_shell/e_desktop_shell_protocol.h
@@ -24,7 +24,7 @@ extern const struct wl_interface xdg_popup_interface;
 #  define XDG_SHELL_VERSION_ENUM
 enum xdg_shell_version
 {
-   XDG_SHELL_VERSION_CURRENT = 3,
+   XDG_SHELL_VERSION_CURRENT = 4,
 };
 # endif
 
@@ -37,6 +37,8 @@ struct xdg_shell_interface
 };
 
 # define XDG_SHELL_PING 0
+# define XDG_SHELL_PING_SINCE_VERSION 1
+
 static inline void 
 xdg_shell_send_ping(struct wl_resource *resource_, uint32_t serial)
 {
@@ -65,53 +67,39 @@ enum xdg_surface_state
 {
    XDG_SURFACE_STATE_MAXIMIZED = 1,
    XDG_SURFACE_STATE_FULLSCREEN = 2,
+   XDG_SURFACE_STATE_RESIZING = 3,
+   XDG_SURFACE_STATE_ACTIVATED = 4,
 };
 # endif
 
 struct xdg_surface_interface
 {
    void (*destroy)(struct wl_client *client, struct wl_resource *resource);
-   void (*transient_for_set)(struct wl_client *client, struct wl_resource *resource, struct wl_resource *parent_resource);
-   void (*margin_set)(struct wl_client *client, struct wl_resource *resource, int32_t l, int32_t r, int32_t t, int32_t b);
+   void (*parent_set)(struct wl_client *client, struct wl_resource *resource, struct wl_resource *parent_resource);
    void (*title_set)(struct wl_client *client, struct wl_resource *resource, const char *title);
    void (*app_id_set)(struct wl_client *client, struct wl_resource *resource, const char *id);
+   void (*window_menu_show)(struct wl_client *client, struct wl_resource *resource, struct wl_resource *seat, uint32_t serial, int32_t x, int32_t y);
    void (*move)(struct wl_client *client, struct wl_resource *resource, struct wl_resource *seat_resource, uint32_t serial);
    void (*resize)(struct wl_client *client, struct wl_resource *resource, struct wl_resource *seat_resource, uint32_t serial, uint32_t edges);
-   void (*output_set)(struct wl_client *client, struct wl_resource *resource, struct wl_resource *output_resource);
-   void (*state_change_request)(struct wl_client *client, struct wl_resource *resource, uint32_t state, uint32_t value, uint32_t serial);
-   void (*state_change_acknowledge)(struct wl_client *client, struct wl_resource *resource, uint32_t state, uint32_t value, uint32_t serial);
-   void (*minimize)(struct wl_client *client, struct wl_resource *resource);
+   void (*ack_configure)(struct wl_client *client, struct wl_resource *resource, uint32_t serial);
+   void (*window_geometry_set)(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t w, int32_t h);
+   void (*maximized_set)(struct wl_client *client, struct wl_resource *resource);
+   void (*maximized_unset)(struct wl_client *client, struct wl_resource *resource);
+   void (*fullscreen_set)(struct wl_client *client, struct wl_resource *resource, struct wl_resource *output);
+   void (*fullscreen_unset)(struct wl_client *client, struct wl_resource *resource);
+   void (*minimized_set)(struct wl_client *client, struct wl_resource *resource);
 };
 
 # define XDG_SURFACE_CONFIGURE 0
-# define XDG_SURFACE_CHANGE_STATE 1
-# define XDG_SURFACE_ACTIVATED 2
-# define XDG_SURFACE_DEACTIVATED 3
-# define XDG_SURFACE_CLOSE 4
+# define XDG_SURFACE_CLOSE 1
+# define XDG_SURFACE_CONFIGURE_SINCE_VERSION 1
+# define XDG_SURFACE_CLOSE_SINCE_VERSION 1
 
 static inline void 
-xdg_surface_send_configure(struct wl_resource *resource_, int32_t w, int32_t h)
+xdg_surface_send_configure(struct wl_resource *resource_, int32_t w, int32_t h, struct wl_array *states, uint32_t serial)
 {
-   wl_resource_post_event(resource_, XDG_SURFACE_CONFIGURE, w, h);
+   wl_resource_post_event(resource_, XDG_SURFACE_CONFIGURE, w, h, states, serial);
 }
-
-static inline void 
-xdg_surface_send_state_change(struct wl_resource *resource_, uint32_t state, uint32_t value, uint32_t serial)
-{
-   wl_resource_post_event(resource_, XDG_SURFACE_CHANGE_STATE, state, value, serial);
-}
-
-static inline void 
-xdg_surface_send_activated(struct wl_resource *resource_)
-{
-   wl_resource_post_event(resource_, XDG_SURFACE_ACTIVATED);
-};
-
-static inline void 
-xdg_surface_send_deactivated(struct wl_resource *resource_)
-{
-   wl_resource_post_event(resource_, XDG_SURFACE_DEACTIVATED);
-};
 
 static inline void 
 xdg_surface_send_close(struct wl_resource *resource_)
@@ -124,11 +112,13 @@ struct xdg_popup_interface
    void (*destroy)(struct wl_client *client, struct wl_resource *resource);
 };
 
-# define XDG_POPUP_DONE 0
+# define XDG_POPUP_POPUP_DONE 0
+# define XDG_POPUP_POPUP_DONE_SINCE_VERSION 1
+
 static inline void 
-xdg_popup_send_done(struct wl_resource *resource_, uint32_t serial)
+xdg_popup_send_popup_done(struct wl_resource *resource_, uint32_t serial)
 {
-   wl_resource_post_event(resource_, XDG_POPUP_DONE, serial);
+   wl_resource_post_event(resource_, XDG_POPUP_POPUP_DONE, serial);
 };
 
 # ifdef __cplusplus
