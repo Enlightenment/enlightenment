d54d6640e89b97a11d063fdf992d38344597aeae
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-02-09 20:43:30 -0500

    don't set client shape rects based on wl opaque regions

--- src/bin/e_comp_wl.c
+++ src/bin/e_comp_wl.c
@@ -2681,40 +2681,6 @@ e_comp_wl_surface_commit(E_Client *ec)
 
         if ((src = eina_tiler_intersection(ec->comp_data->pending.opaque, tmp)))
           {
-             Eina_Rectangle *rect;
-             Eina_Iterator *itr;
-             int i = 0;
-
-             ec->shape_rects_num = 0;
-
-             itr = eina_tiler_iterator_new(src);
-             EINA_ITERATOR_FOREACH(itr, rect)
-               ec->shape_rects_num += 1;
-
-             ec->shape_rects = 
-               malloc(sizeof(Eina_Rectangle) * ec->shape_rects_num);
-
-             if (ec->shape_rects)
-               {
-                  EINA_ITERATOR_FOREACH(itr, rect)
-                    {
-                       E_RECTS_CLIP_TO_RECT(rect->x, rect->y, rect->w, rect->h, 
-                                            0, 0, ec->client.w, ec->client.h);
-
-                       ec->shape_rects[i] = *(Eina_Rectangle *)((char *)rect);
-
-                       ec->shape_rects[i].x = rect->x;
-                       ec->shape_rects[i].y = rect->y;
-                       ec->shape_rects[i].w = rect->w;
-                       ec->shape_rects[i].h = rect->h;
-
-                       i++;
-                    }
-
-                  ec->need_shape_export = EINA_TRUE;
-               }
-
-             eina_iterator_free(itr);
              eina_tiler_free(src);
           }
 
