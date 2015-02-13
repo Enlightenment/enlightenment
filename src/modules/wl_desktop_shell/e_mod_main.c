92079a11e75f6acccc7219a9caadc80f3d58f00f
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-02-13 15:45:03 -0500

    allow focus on normal wl clients again

--- src/modules/wl_desktop_shell/e_mod_main.c
+++ src/modules/wl_desktop_shell/e_mod_main.c
@@ -1196,6 +1196,7 @@ _e_xdg_shell_cb_surface_get(struct wl_client *client, struct wl_resource *resour
    cdata->shell.unmap = _e_xdg_shell_surface_unmap;
 
    /* set toplevel client properties */
+   ec->icccm.accepts_focus = 1;
    ec->borderless = !ec->internal_elm_win;
    ec->lock_border = EINA_TRUE;
    ec->border.changed = ec->changes.border = !ec->borderless;
