ca6c46d0c82335be0aa4c44c7e68f375ae590b00
Chris Michael <cp.michael@samsung.com>
2015-04-06 15:59:59 -0400

    wl-drm: Hook into ecore_evas_resize callback to update comp canvas
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/modules/wl_drm/e_mod_main.c
+++ src/modules/wl_drm/e_mod_main.c
@@ -79,6 +79,12 @@ end:
    return ECORE_CALLBACK_PASS_ON;
 }
 
+static void
+_e_mod_drm_cb_ee_resize(Ecore_Evas *ee EINA_UNUSED)
+{
+   e_comp_canvas_update();
+}
+
 EAPI void *
 e_modapi_init(E_Module *m)
 {
@@ -117,13 +123,15 @@ e_modapi_init(E_Module *m)
         return NULL;
      }
 
+   ecore_evas_data_set(e_comp->ee, "comp", e_comp);
+
    /* get the current screen geometry */
    ecore_evas_screen_geometry_get(e_comp->ee, NULL, NULL, &w, &h);
 
    /* resize the canvas */
    ecore_evas_resize(e_comp->ee, w, h);
 
-   /* TODO: hook ecore_evas_callback_resize_set */
+   ecore_evas_callback_resize_set(e_comp->ee, _e_mod_drm_cb_ee_resize);
 
    if (!e_xinerama_fake_screens_exist())
      {
