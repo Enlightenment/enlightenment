Enlightenment/enlightenment

commit 99589eb5af346add9fe912a768ed006416cb2220

remove E_Win+e_canvas, convert all internal wins to use elm_win
known issues:

* keybindings for efm windows (eg. ctrl+w) are broken until the rest of the conversion is completed

src/bin/e_obj_dialog.c
@@ -2,9 +2,8 @@
 
 /* local subsystem functions */
 static void _e_obj_dialog_free(E_Obj_Dialog *od);
-static void _e_obj_dialog_cb_delete(E_Win *win);
+static void _e_obj_dialog_cb_delete(E_Obj_Dialog *od, Evas *e EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED);
 static void _e_obj_dialog_cb_close(void *data, Evas_Object *obj, const char *emission, const char *source);
-static void _e_obj_dialog_cb_resize(E_Win *win);
 
 /* local subsystem globals */
 
@@ -16,7 +15,7 @@ _key_down_cb(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *
    Evas_Event_Key_Down *ev = event;
 
    if (!strcmp(ev->key, "Escape") && data)
-     _e_obj_dialog_cb_delete(data);
+     _e_obj_dialog_cb_delete(data, NULL, NULL, NULL);
 }
 
 EAPI E_Obj_Dialog *
@@ -30,23 +29,21 @@ e_obj_dialog_new(E_Comp *c, char *title, char *class_name, char *class_class)
    if (!c) c = e_util_comp_current_get();
    od = E_OBJECT_ALLOC(E_Obj_Dialog, E_OBJ_DIALOG_TYPE, _e_obj_dialog_free);
    if (!od) return NULL;
-   od->win = e_win_new(c);
+   od->win = elm_win_add(NULL, NULL, ELM_WIN_DIALOG_BASIC);
    if (!od->win)
      {
         free(od);
         return NULL;
      }
-   e_win_delete_callback_set(od->win, _e_obj_dialog_cb_delete);
-   e_win_resize_callback_set(od->win, _e_obj_dialog_cb_resize);
-   od->win->data = od;
-   e_win_dialog_set(od->win, 1);
-   e_win_name_class_set(od->win, class_name, class_class);
-   e_win_title_set(od->win, title);
+   evas_object_event_callback_add(od->win, EVAS_CALLBACK_DEL, (Evas_Object_Event_Cb)_e_obj_dialog_cb_delete, od);
+   ecore_evas_name_class_set(e_win_ee_get(od->win), class_name, class_class);
+   elm_win_title_set(od->win, title);
 
-   o = edje_object_add(e_win_evas_get(od->win));
+   o = edje_object_add(evas_object_evas_get(od->win));
+   elm_win_resize_object_add(od->win, o);
    od->bg_object = o;
 
-   e_win_centered_set(od->win, 1);
+   elm_win_center(od->win, 1, 1);
    od->cb_delete = NULL;
 
    mask = 0;
@@ -70,7 +67,7 @@ e_obj_dialog_icon_set(E_Obj_Dialog *od, char *icon)
 {
    E_OBJECT_CHECK(od);
    E_OBJECT_TYPE_CHECK(od, E_OBJ_DIALOG_TYPE);
-   eina_stringshare_replace(&od->win->client->internal_icon, icon);
+   e_win_client_icon_set(od->win, icon);
  }
 
 EAPI void
@@ -85,22 +82,19 @@ e_obj_dialog_show(E_Obj_Dialog *od)
    edje_object_size_min_get(od->bg_object, &mw, &mh);
    edje_object_size_min_restricted_calc(od->bg_object, &mw, &mh, mw, mh);
    evas_object_resize(od->bg_object, mw, mh);
-   e_win_resize(od->win, mw, mh);
-   e_win_size_min_set(od->win, mw, mh);
+   evas_object_resize(od->win, mw, mh);
+   evas_object_size_hint_min_set(od->win, mw, mh);
    edje_object_size_max_get(od->bg_object, &w, &h);
    if ((w > 0) && (h > 0))
      {
         if (w < mw) w = mw;
         if (h < mh) h = mh;
-        e_win_size_max_set(od->win, w, h);
+        evas_object_size_hint_max_set(od->win, w, h);
      }
    s = edje_object_data_get(od->bg_object, "borderless");
    if (s && (!strcmp(s, "1")))
-     e_win_borderless_set(od->win, 1);
-   s = edje_object_data_get(od->bg_object, "shaped");
-   if (s && (!strcmp(s, "1")))
-     e_win_shaped_set(od->win, 1);
-   e_win_show(od->win);
+     elm_win_borderless_set(od->win, 1);
+   evas_object_show(od->win);
 }
 
 EAPI void
@@ -129,16 +123,13 @@ static void
 _e_obj_dialog_free(E_Obj_Dialog *od)
 {
    if (od->bg_object) evas_object_del(od->bg_object);
-   e_object_del(E_OBJECT(od->win));
+   evas_object_del(od->win);
    free(od);
 }
 
 static void
-_e_obj_dialog_cb_delete(E_Win *win)
+_e_obj_dialog_cb_delete(E_Obj_Dialog *od, Evas *e EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-   E_Obj_Dialog *od;
-
-   od = win->data;
    if (od->cb_delete)
      od->cb_delete(od);
    e_object_del(E_OBJECT(od));
@@ -154,13 +145,3 @@ _e_obj_dialog_cb_close(void *data, Evas_Object *obj __UNUSED__, const char *emis
      od->cb_delete(od);
    e_util_defer_object_del(E_OBJECT(od));
 }
-
-static void
-_e_obj_dialog_cb_resize(E_Win *win)
-{
-   E_Obj_Dialog *od;
-
-   od = win->data;
-   evas_object_resize(od->bg_object, od->win->w, od->win->h);
-}
-

commit b637a4f586a928c23990e0c8baa85260743f94a3

Bugfix: e dialog: don’t crash when using the escape key to close.
 Commit 99589eb5af346add9fe912a768ed006416cb2220 introduced this bug as the callback creation wasn’t changed to suit the new prototype and stil passed a window object.

src/bin/e_obj_dialog.c
@@ -50,7 +50,7 @@ e_obj_dialog_new(char *title, char *class_name, char *class_class)
    if (!kg)
      fprintf(stderr, "ERROR: unable to redirect \"Escape\" key events to object %p.\n", o);
    evas_object_event_callback_add(o, EVAS_CALLBACK_KEY_DOWN,
-                                  _key_down_cb, od->win);
+                                  _key_down_cb, od);
 
    return od;
 }