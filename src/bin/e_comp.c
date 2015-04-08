a73d2583c5308e93de3ecd5c46b56c1d5076a5a2
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-04-08 14:56:05 -0400

    don't crash on wl shutdown
    devilhorns? more like crasherhorns imo

--- src/bin/e_comp.c
+++ src/bin/e_comp.c
@@ -1277,6 +1277,9 @@ e_comp_internal_save(void)
 EINTERN int
 e_comp_shutdown(void)
 {
+#ifdef HAVE_WAYLAND
+   E_Pixmap_Type type = e_comp->comp_type;
+#endif
    E_FREE_FUNC(action_timeout, ecore_timer_del);
    while (e_comp->clients)
      e_object_del(eina_list_data_get(e_comp->clients));
@@ -1286,8 +1289,6 @@ e_comp_shutdown(void)
    E_FREE_LIST(hooks, e_client_hook_del);
 
 #ifdef HAVE_WAYLAND
-   E_Pixmap_Type type = e_comp->comp_type;
-
    if (type == E_PIXMAP_TYPE_WL)
      e_comp_wl_shutdown();
 #endif
