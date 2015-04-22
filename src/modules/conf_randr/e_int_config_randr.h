e7219bab37be54702af75f63103b44415fb028c1
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-04-22 18:28:23 -0400

    remove dead conf_randr files

--- src/modules/conf_randr/e_int_config_randr.h
+++
@@ -1,9 +0,0 @@
-#ifdef E_TYPEDEFS
-#else
-# ifndef E_INT_CONFIG_RANDR_H
-#  define E_INT_CONFIG_RANDR_H
-
-E_Config_Dialog *e_int_config_randr(Evas_Object *parent, const char *params EINA_UNUSED);
-
-# endif
-#endif
