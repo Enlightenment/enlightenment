3e20ea2fbc123d53283f6532a0d3772f3c10bad5
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-04-28 13:25:18 -0400

    fix xwayland linkage + makefile formatting

--- src/modules/Makefile_xwayland.mk
+++ src/modules/Makefile_xwayland.mk
@@ -4,14 +4,16 @@ xwaylandpkg_LTLIBRARIES = src/modules/xwayland/module.la
 
 src_modules_xwayland_module_la_DEPENDENCIES = $(MDEPENDENCIES)
 src_modules_xwayland_module_la_CPPFLAGS  = \
-  $(MOD_CPPFLAGS) @XWAYLAND_CFLAGS@ @WAYLAND_CFLAGS@ \
-  -DXWAYLAND_BIN=\"@XWAYLAND_BIN@\"
+$(MOD_CPPFLAGS) \
+@XWAYLAND_CFLAGS@ \
+@WAYLAND_CFLAGS@ \
+-DXWAYLAND_BIN=\"@XWAYLAND_BIN@\"
 
-src_modules_xwayland_module_la_LIBADD   = $(LIBS) @XWAYLAND_LIBS@ @WAYLAND_LIBS@
+src_modules_xwayland_module_la_LIBADD   = $(MOD_LIBS) @XWAYLAND_LIBS@ @WAYLAND_LIBS@
 src_modules_xwayland_module_la_LDFLAGS = $(MOD_LDFLAGS)
 
 src_modules_xwayland_module_la_SOURCES = \
-  src/modules/xwayland/e_mod_main.c
+src/modules/xwayland/e_mod_main.c
 
 PHONIES += xwayland install-xwayland
 xwayland: $(xwaylandpkg_LTLIBRARIES)
