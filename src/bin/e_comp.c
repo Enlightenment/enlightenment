Enlightenment/enlightenment

commit e24666b9296d1972f320d89218ce9990e9c44420

fix comp input ungrabbing to read more clearly and not pop nocomp wrongly

src/bin/e_comp.c
@@ -1568,16 +1568,16 @@ e_comp_ungrab_input(Eina_Bool mouse, Eina_Bool kbd)
 
    mouse = !!mouse;
    kbd = !!kbd;
-   if (mouse && (e_comp->input_mouse_grabs == 1))
-     mwin = e_comp->ee_win;
-   if (kbd && (e_comp->input_key_grabs == 1))
-     kwin = e_comp->ee_win;
    if (e_comp->input_mouse_grabs)
      e_comp->input_mouse_grabs -= mouse;
    if (e_comp->input_key_grabs)
      e_comp->input_key_grabs -= kbd;
-   e_comp_override_timed_pop();
+   if (mouse && (!e_comp->input_mouse_grabs))
+     mwin = e_comp->ee_win;
+   if (kbd && (!e_comp->input_key_grabs))
+     kwin = e_comp->ee_win;
    if ((!mwin) && (!kwin)) return;
+   e_comp_override_timed_pop();
    e_grabinput_release(mwin, kwin);
    evas_event_feed_mouse_out(e_comp->evas, 0, NULL);
    evas_event_feed_mouse_in(e_comp->evas, 0, NULL);

commit e717a2e107aad324b734d4a3916d8a9a57c07f50

fix nocomp popping on comp input ungrabbing
regression from e24666b9296d1972f320d89218ce9990e9c44420

src/bin/e_comp.c
@@ -1576,8 +1576,8 @@ e_comp_ungrab_input(Eina_Bool mouse, Eina_Bool kbd)
      mwin = e_comp->ee_win;
    if (kbd && (!e_comp->input_key_grabs))
      kwin = e_comp->ee_win;
-   if ((!mwin) && (!kwin)) return;
    e_comp_override_timed_pop();
+   if ((!mwin) && (!kwin)) return;
    e_grabinput_release(mwin, kwin);
    evas_event_feed_mouse_out(e_comp->evas, 0, NULL);
    evas_event_feed_mouse_in(e_comp->evas, 0, NULL);