83eebacb5d6811daa94782f7fd5a6ff52ba57fe0
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-05-18 13:19:56 -0400

    allow new fullscreening clients to replace current nocomp where applicable

--- src/bin/e_client.c
+++ src/bin/e_client.c
@@ -3823,6 +3823,8 @@ e_client_fullscreen(E_Client *ec, E_Fullscreen policy)
         ec->need_fullscreen = 1;
         return;
      }
+   if (e_comp->nocomp_ec && (ec->desk == e_comp->nocomp_ec->desk))
+     e_comp->nocomp_ec = ec;
    ec->desk->fullscreen_clients = eina_list_append(ec->desk->fullscreen_clients, ec);
    ec->pre_res_change.valid = 0;
 
