8e91f32978bf2ccf5d15a6952eb5ae2a6e932509
Derek Foreman <derekf@osg.samsung.com>
2015-07-03 11:55:40 -0400

    wizard: Prevent crash
    Summary: If we get here when curpage is NULL, we'll crash later, so we should test for it.
    
    Reviewers: zmike
    
    Subscribers: cedric
    
    Differential Revision: https://phab.enlightenment.org/D2793

--- src/modules/wizard/e_wizard.c
+++ src/modules/wizard/e_wizard.c
@@ -406,7 +406,7 @@ static Eina_Bool
 _e_wizard_cb_icons_update(void *data EINA_UNUSED, int ev_type EINA_UNUSED, void *ev EINA_UNUSED)
 {
    got_icons = EINA_TRUE;
-   if (_e_wizard_check_xdg())
+   if (curpage && _e_wizard_check_xdg())
      _e_wizard_next_xdg();
    return ECORE_CALLBACK_PASS_ON;
 }
