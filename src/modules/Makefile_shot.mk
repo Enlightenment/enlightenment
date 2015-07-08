00b52c39511a209b0b3d32643d2c50ca7cf470e2
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-07-08 17:11:16 -0400

    make wayland-specific shot protocol files conditional on wayland build

--- src/modules/Makefile_shot.mk
+++ src/modules/Makefile_shot.mk
@@ -13,12 +13,16 @@ shotpkg_LTLIBRARIES = src/modules/shot/module.la
 
 src_modules_shot_module_la_LIBADD = $(MOD_LIBS) @WAYLAND_LIBS@
 src_modules_shot_module_la_CPPFLAGS = \
-  $(MOD_CPPFLAGS) @WAYLAND_CFLAGS@
+$(MOD_CPPFLAGS) @WAYLAND_CFLAGS@
 src_modules_shot_module_la_LDFLAGS = $(MOD_LDFLAGS)
 src_modules_shot_module_la_SOURCES = \
-  src/modules/shot/e_mod_main.c \
-  src/modules/shot/screenshooter-client-protocol.c \
-  src/modules/shot/screenshooter-client-protocol.h
+src/modules/shot/e_mod_main.c
+
+if HAVE_WAYLAND
+src_modules_shot_module_la_SOURCES += \
+src/modules/shot/screenshooter-client-protocol.c \
+src/modules/shot/screenshooter-client-protocol.h
+endif
 
 PHONIES += shot install-shot
 shot: $(shotpkg_LTLIBRARIES) $(shot_DATA)
