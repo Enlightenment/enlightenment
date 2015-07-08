697e8b32df09a01ed2632cd688a5b5d8bd5991b3
Chris Michael <cp.michael@samsung.com>
2015-07-08 15:07:49 -0400

    shot: Update shot makefile to include client screenshot protocol files
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/modules/Makefile_shot.mk
+++ src/modules/Makefile_shot.mk
@@ -1,18 +1,24 @@
 EXTRA_DIST += src/modules/shot/module.desktop.in \
-src/modules/shot/e-module-shot.edj
+src/modules/shot/e-module-shot.edj \
+src/modules/shot/screenshooter-client-protocol.h \
+src/modules/shot/screenshooter-client-protocol.c
+
 if USE_MODULE_SHOT
 shotdir = $(MDIR)/shot
 shot_DATA = src/modules/shot/e-module-shot.edj \
 	    src/modules/shot/module.desktop
 
-
 shotpkgdir = $(MDIR)/shot/$(MODULE_ARCH)
 shotpkg_LTLIBRARIES = src/modules/shot/module.la
 
-src_modules_shot_module_la_LIBADD = $(MOD_LIBS)
-src_modules_shot_module_la_CPPFLAGS = $(MOD_CPPFLAGS)
+src_modules_shot_module_la_LIBADD = $(MOD_LIBS) @WAYLAND_LIBS@
+src_modules_shot_module_la_CPPFLAGS = \
+  $(MOD_CPPFLAGS) @WAYLAND_CFLAGS@ -DNEED_WL
 src_modules_shot_module_la_LDFLAGS = $(MOD_LDFLAGS)
-src_modules_shot_module_la_SOURCES = src/modules/shot/e_mod_main.c
+src_modules_shot_module_la_SOURCES = \
+  src/modules/shot/e_mod_main.c \
+  src/modules/shot/screenshooter-client-protocol.c \
+  src/modules/shot/screenshooter-client-protocol.h
 
 PHONIES += shot install-shot
 shot: $(shotpkg_LTLIBRARIES) $(shot_DATA)
