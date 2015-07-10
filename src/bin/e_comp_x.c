f3f29a0cf83412ae4f09fb1fbed065daca7c4047
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-07-10 16:59:47 -0400

    delete render updates for xwayland clients upon triggering a client resize
    fixes frame dropping during resize

--- src/bin/e_comp_x.c
+++ src/bin/e_comp_x.c
@@ -931,8 +931,7 @@ _e_comp_x_evas_resize_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_i
      }
 
    ec->post_resize = 1;
-   if (e_pixmap_is_x(ec->pixmap))
-     e_comp_object_render_update_del(ec->frame);
+   e_comp_object_render_update_del(ec->frame);
    _e_comp_x_post_client_idler_add(ec);
 }
 
