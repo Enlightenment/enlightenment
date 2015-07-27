0a8ee7b5adc6eac604f6f007bfa031f74a4c4c4e
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-07-27 13:23:13 -0400

    allow x11 mouse wheel events to check ev->event_window for possible client match
    in the event that these windows are different, event_window is the parent of window
    which may or may not be explicitly tracked by an E_Client, so the wheel events here
    should be sent to the parent as is done in mouse button events
    
    fix T2604

--- src/bin/e_comp_x.c
+++ src/bin/e_comp_x.c
@@ -2218,6 +2218,8 @@ _e_comp_x_mouse_wheel(void *d EINA_UNUSED, int t EINA_UNUSED, Ecore_Event_Mouse_
    //else
      {
         ec = _e_comp_x_client_find_by_window(ev->window);
+        if ((!ec) && (ev->window != ev->event_window))
+          ec = _e_comp_x_client_find_by_window(ev->event_window);
         if (!ec) return ECORE_CALLBACK_RENEW;
      }
    if (_e_comp_x_client_data_get(ec)->deleted) return ECORE_CALLBACK_RENEW;
