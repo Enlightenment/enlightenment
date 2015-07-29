1496b3db7b16afd2f1d5b2ebf3e29e761cff63dd
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-07-29 15:38:17 -0400

    don't reset wayland surface geometry for overrides

--- src/bin/e_comp_wl.c
+++ src/bin/e_comp_wl.c
@@ -1087,7 +1087,7 @@ _e_comp_wl_surface_state_commit(E_Client *ec, E_Comp_Wl_Surface_State *state)
 
         if (ec->new_client)
           ec->placed = placed;
-        else if ((first) && (ec->placed) && (!ec->internal))
+        else if ((first) && (ec->placed) && (!ec->internal) && (!ec->override))
           {
              ec->x = ec->y = 0;
              ec->placed = EINA_FALSE;
