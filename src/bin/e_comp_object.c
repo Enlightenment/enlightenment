01ba4eea555cc475ff6736f3a799672dfe110f74
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-07-29 15:25:22 -0400

    end compositor animations for dead clients on hide
    fixes a huge object leak in xwayland related to menus

--- src/bin/e_comp_object.c
+++ src/bin/e_comp_object.c
@@ -2052,6 +2052,7 @@ _e_comp_smart_hide(Evas_Object *obj)
              evas_object_event_callback_del_full(o, EVAS_CALLBACK_DEL, _e_comp_object_cb_mirror_del, cw);
              evas_object_del(o);
           }
+        if (!_e_comp_object_animating_end(cw)) return;
      }
    if (stopping) return;
    if (!cw->ec->input_only)
