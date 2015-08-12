9613b1faff951c9087e87cca56292a00c02f8d1c
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-08-12 13:25:30 -0400

    do not add render update on resize fail due to unusable pixmap in wl
    this will not have any effect

--- src/bin/e_comp_object.c
+++ src/bin/e_comp_object.c
@@ -992,6 +992,7 @@ _e_comp_intercept_resize(void *data, Evas_Object *obj, int w, int h)
      }
    if ((!cw->ec->input_only) && (!e_pixmap_size_get(cw->ec->pixmap, &pw, &ph)))
      {
+        if (e_comp->comp_type != E_PIXMAP_TYPE_X) return;
         /* client can't be resized if its pixmap isn't usable, try again */
         e_pixmap_dirty(cw->ec->pixmap);
         e_comp_object_render_update_add(obj);
