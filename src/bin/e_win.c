67524ac92152638550d1943be5245e1df8e29942
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-09-21 16:51:41 -0400

    re-set internal ee win after applying alpha
    changing a window's alpha causes it to be recreated in the engine

--- src/bin/e_win.c
+++ src/bin/e_win.c
@@ -318,6 +318,7 @@ e_win_new(E_Comp *c)
                                   &win->evas_win);
    e_canvas_add(win->ecore_evas);
    ecore_evas_alpha_set(win->ecore_evas, 1);
+   win->evas_win = ecore_evas_window_get(win->ecore_evas);
    ecore_evas_data_set(win->ecore_evas, "E_Win", win);
    ecore_evas_callback_move_set(win->ecore_evas, _e_win_cb_move);
    ecore_evas_callback_resize_set(win->ecore_evas, _e_win_cb_resize);
