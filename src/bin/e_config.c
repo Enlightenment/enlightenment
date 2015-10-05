a6408d95a604d0ea5b0eb69771c7fa4cee55a099
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-10-05 15:56:28 -0400

    attempt to load default e_bindings if initial load fails

--- src/bin/e_config.c
+++ src/bin/e_config.c
@@ -1085,7 +1085,7 @@ e_config_load(void)
    else
      e_bindings = e_config_domain_load("e_bindings", _e_config_binding_edd);
 
-   if (e_bindings && (e_bindings->config_version != E_CONFIG_BINDINGS_VERSION))
+   if ((!e_bindings) || (e_bindings->config_version != E_CONFIG_BINDINGS_VERSION))
      {
         Eina_Stringshare *prof;
 
