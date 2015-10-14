2814066c39aada30369a0cda04960eb87fdb4521
Chris Michael <cp.michael@samsung.com>
2015-10-14 10:22:19 -0400

    enlightenment: Fix E_CONFIG_LIMIT to use the proper min & max values
    e_config->backlight.idle_dim is actually an unsigned char in the
    structure, so use a 0 & 1 for min & max
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/e_config.c
+++ src/bin/e_config.c
@@ -1502,7 +1502,7 @@ e_config_load(void)
 
    E_CONFIG_LIMIT(e_config->backlight.normal, 0.05, 1.0);
    E_CONFIG_LIMIT(e_config->backlight.dim, 0.05, 1.0);
-   E_CONFIG_LIMIT(e_config->backlight.idle_dim, 0.05, 1.0);
+   E_CONFIG_LIMIT(e_config->backlight.idle_dim, 0, 1);
 
    if (!e_config->icon_theme)
      e_config->icon_theme = eina_stringshare_add("hicolor");  // FDO default
