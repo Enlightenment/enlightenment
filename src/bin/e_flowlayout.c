71f77206697bf6a6d856fe43b901c7dcb932e928
Amitesh Singh <amitesh.sh@samsung.com>
2015-10-14 14:02:07 -0400

    e_flowlayout: typecast return value to int
    Summary:
    return value is small enough, so there is no risk in casting from unsigned int to signed int.
    
    CID: 1267211
    
    Reviewers: devilhorns, raster, cedric, zmike
    
    Subscribers: seoz, sachin.dev
    
    Differential Revision: https://phab.enlightenment.org/D3179

--- src/bin/e_flowlayout.c
+++ src/bin/e_flowlayout.c
@@ -233,7 +233,7 @@ e_flowlayout_pack_end(Evas_Object *obj, Evas_Object *child)
    sd->items = eina_list_append(sd->items, child);
    sd->changed = 1;
    if (sd->frozen <= 0) _e_flowlayout_smart_reconfigure(sd);
-   return eina_list_count(sd->items) - 1;
+   return (int)(eina_list_count(sd->items) - 1);
 }
 
 E_API int
@@ -290,7 +290,7 @@ e_flowlayout_pack_count_get(Evas_Object *obj)
    if (evas_object_smart_smart_get(obj) != _e_smart) SMARTERR(0);
    sd = evas_object_smart_data_get(obj);
    if (!sd) return 0;
-   return eina_list_count(sd->items);
+   return (int)eina_list_count(sd->items);
 }
 
 E_API Evas_Object *
