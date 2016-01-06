11cc32b7a6e4d99b160149a0674d2150fc39ad73
Mike Blumenkrantz <zmike@osg.samsung.com>
2016-01-06 13:02:41 -0500

    remove wayland function usage from grabinput methods
    these never did anythng useful and no longer even compile

--- src/bin/e_grabinput.c
+++ src/bin/e_grabinput.c
@@ -35,9 +35,6 @@ e_grabinput_get(Ecore_Window mouse_win, int confine_mouse, Ecore_Window key_win)
 #ifndef HAVE_WAYLAND_ONLY
         if (e_comp->comp_type == E_PIXMAP_TYPE_X)
           ecore_x_pointer_ungrab();
-#else
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          ecore_wl_input_ungrab(ecore_wl_input_get());
 #endif
         grab_mouse_win = 0;
      }
@@ -46,9 +43,6 @@ e_grabinput_get(Ecore_Window mouse_win, int confine_mouse, Ecore_Window key_win)
 #ifndef HAVE_WAYLAND_ONLY
         if (e_comp->comp_type == E_PIXMAP_TYPE_X)
           ecore_x_keyboard_ungrab();
-#else
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          ecore_wl_input_ungrab(ecore_wl_input_get());
 #endif
 
         grab_key_win = 0;
@@ -65,14 +59,6 @@ e_grabinput_get(Ecore_Window mouse_win, int confine_mouse, Ecore_Window key_win)
              else
                ret = ecore_x_pointer_grab(mouse_win);
              if (!ret) return 0;
-          }
-#else
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          {
-             Ecore_Wl_Window *wl_win;
-
-             if ((wl_win = ecore_wl_window_find(mouse_win)))
-               ecore_wl_input_grab(ecore_wl_input_get(), wl_win, 0);
           }
 #endif
         grab_mouse_win = mouse_win;
@@ -95,14 +81,6 @@ e_grabinput_get(Ecore_Window mouse_win, int confine_mouse, Ecore_Window key_win)
                   return 0;
                }
           }
-#else
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          {
-             Ecore_Wl_Window *wl_win;
-
-             if ((wl_win = ecore_wl_window_find(key_win)))
-               ecore_wl_input_grab(ecore_wl_input_get(), wl_win, 0);
-          }
 #endif
         grab_key_win = key_win;
      }
@@ -120,9 +98,6 @@ e_grabinput_release(Ecore_Window mouse_win, Ecore_Window key_win)
 #ifndef HAVE_WAYLAND_ONLY
         if (e_comp->comp_type == E_PIXMAP_TYPE_X)
           ecore_x_pointer_ungrab();
-#else
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          ecore_wl_input_ungrab(ecore_wl_input_get());
 #endif
 
         grab_mouse_win = 0;
@@ -132,9 +107,6 @@ e_grabinput_release(Ecore_Window mouse_win, Ecore_Window key_win)
 #ifndef HAVE_WAYLAND_ONLY
         if (e_comp->comp_type == E_PIXMAP_TYPE_X)
           ecore_x_keyboard_ungrab();
-#else
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          ecore_wl_input_ungrab(ecore_wl_input_get());
 #endif
 
         grab_key_win = 0;
@@ -191,10 +163,6 @@ e_grabinput_mouse_win_get(void)
 static void
 _e_grabinput_focus_do(Ecore_Window win, E_Focus_Method method)
 {
-#ifdef HAVE_WAYLAND
-   Ecore_Wl_Window *wl_win;
-#endif
-
    /* fprintf(stderr, "focus to %x method %i\n", win, method); */
    switch (method)
      {
@@ -209,29 +177,12 @@ _e_grabinput_focus_do(Ecore_Window win, E_Focus_Method method)
              ecore_x_icccm_take_focus_send(win, ecore_x_current_time_get());
           }
 #endif
-#ifdef HAVE_WAYLAND
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          {
-             if ((wl_win = ecore_wl_window_find(win)))
-               {
-                  /* FIXME: Need to add an ecore_wl_window_focus function */
-               }
-          }
-#endif
         break;
 
       case E_FOCUS_METHOD_GLOBALLY_ACTIVE:
 #ifndef HAVE_WAYLAND_ONLY
         if (e_comp_util_has_x())
           ecore_x_icccm_take_focus_send(win, ecore_x_current_time_get());
-#else
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          {
-             if ((wl_win = ecore_wl_window_find(win)))
-               {
-                  /* FIXME: Need to add an ecore_wl_window_focus function */
-               }
-          }
 #endif
         break;
 
@@ -239,14 +190,6 @@ _e_grabinput_focus_do(Ecore_Window win, E_Focus_Method method)
 #ifndef HAVE_WAYLAND_ONLY
         if (e_comp_util_has_x())
           ecore_x_window_focus_at_time(win, ecore_x_current_time_get());
-#else
-        if (e_comp->comp_type == E_PIXMAP_TYPE_WL)
-          {
-             if ((wl_win = ecore_wl_window_find(win)))
-               {
-                  /* FIXME: Need to add an ecore_wl_window_focus function */
-               }
-          }
 #endif
         break;
 
