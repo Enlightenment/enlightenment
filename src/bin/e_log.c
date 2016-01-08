2df04042269f3b5604c719844eac372fa5fcddd2
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2016-01-08 18:01:45 +0900

    e logs - the custom e log func breaks eina backtraces, so don't use it
    this fixes e's logs to include eina backtraces again. this is a
    shortcoming of eina_log not being able to do multiple passes basically
    (multiple outputs) per log.

--- src/bin/e_log.c
+++ src/bin/e_log.c
@@ -2,33 +2,10 @@
 
 EINTERN int e_log_dom = -1;
 
-static const char *_names[] = {
-   "CRI",
-   "ERR",
-   "WRN",
-   "INF",
-   "DBG",
-};
-
-static void
-_e_log_cb(const Eina_Log_Domain *d, Eina_Log_Level level, const char *file, const char *fnc EINA_UNUSED, int line, const char *fmt, void *data EINA_UNUSED, va_list args)
-{
-   const char *color;
-
-   color = eina_log_level_color_get(level);
-   fprintf(stdout,
-           "%s%s<" EINA_COLOR_RESET "%s%s>" EINA_COLOR_RESET "%s:%d" EINA_COLOR_RESET " ",
-           color, _names[level > EINA_LOG_LEVEL_DBG ? EINA_LOG_LEVEL_DBG : level],
-           d->domain_str, color, file, line);
-   vfprintf(stdout, fmt, args);
-   putc('\n', stdout);
-}
-
 EINTERN int
 e_log_init(void)
 {
    e_log_dom = eina_log_domain_register("e", EINA_COLOR_WHITE);
-   eina_log_print_cb_set(_e_log_cb, NULL);
    return e_log_dom != -1;
 }
 
