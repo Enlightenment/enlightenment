a707cc3be6c7278959fbe4d3cdc6d2bf8db5dda3
Mike Blumenkrantz <zmike@osg.samsung.com>
2016-01-14 10:41:08 -0500

    remove configs for illume modules
    these have been out of the tree for years but I forgot to remove the
    corresponding configs, leading to error messages when the mobile profile
    attempts to load them

--- config/mobile/Makefile.mk
+++ config/mobile/Makefile.mk
@@ -4,11 +4,6 @@ config/mobile/enlightenment-mobile.png \
 config/mobile/e.src \
 config/mobile/e_bindings.src \
 config/mobile/module.battery.src \
-config/mobile/module.illume-home.src \
-config/mobile/module.illume-keyboard.src \
-config/mobile/module.illume-indicator.src \
-config/mobile/module.illume-softkey.src \
-config/mobile/module.illume2.src \
 config/mobile/module.conf.src
 
 configmobilefilesdir = $(datadir)/enlightenment/data/config/mobile
@@ -18,9 +13,4 @@ config/mobile/enlightenment-mobile.png \
 config/mobile/e.cfg \
 config/mobile/e_bindings.cfg \
 config/mobile/module.battery.cfg \
-config/mobile/module.illume-home.cfg \
-config/mobile/module.illume-keyboard.cfg \
-config/mobile/module.illume-indicator.cfg \
-config/mobile/module.illume-softkey.cfg \
-config/mobile/module.illume2.cfg \
 config/mobile/module.conf.cfg
