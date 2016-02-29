dddf8c5f1e4e51a3221a809a0df43011ec664918
Mike Blumenkrantz <zmike@osg.samsung.com>
2016-02-29 10:07:32 -0500

    prevent potential null deref during pager (plain) window drag
    CID 1237302

--- src/modules/pager_plain/e_mod_main.c
+++ src/modules/pager_plain/e_mod_main.c
@@ -1878,10 +1878,12 @@ _pager_window_cb_mouse_move(void *data, Evas *e EINA_UNUSED, Evas_Object *obj EI
             (resist * resist)) return;
 
         if ((pw->desk) && (pw->desk->pager))
-          pw->desk->pager->dragging = 1;
+          {
+             pw->desk->pager->dragging = 1;
+             edje_object_signal_emit(pw->desk->o_desk, "e,action,drag,in", "e");
+          }
         pw->drag.start = 0;
         e_comp_object_effect_clip(pw->client->frame);
-        edje_object_signal_emit(pw->desk->o_desk, "e,action,drag,in", "e");
         pw->desk->pager->active_drop_pd = pw->desk;
      }
 
