15806cbbf9b3905bec1e8488a4327c798ff2510b
Mike Blumenkrantz <zmike@osg.samsung.com>
2016-03-31 14:06:08 -0400

    don't set wl xdg popups to POPUP layer, set popups as placed on creation
    resolves random popup placements

--- src/modules/wl_desktop_shell/e_mod_main.c
+++ src/modules/wl_desktop_shell/e_mod_main.c
@@ -1250,8 +1250,7 @@ _e_xdg_shell_cb_popup_get(struct wl_client *client, struct wl_resource *resource
      ec->border.changed = ec->changes.border = !ec->borderless;
    ec->changes.icon = !!ec->icccm.class;
    ec->netwm.type = E_WINDOW_TYPE_POPUP_MENU;
-   ec->comp_data->set_win_type = EINA_TRUE;
-   evas_object_layer_set(ec->frame, E_LAYER_CLIENT_POPUP);
+   ec->placed = ec->comp_data->set_win_type = EINA_TRUE;
 
    /* set this client as a transient for parent */
    _e_shell_surface_parent_set(ec, parent_resource);
