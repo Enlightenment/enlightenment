cec35867df61caa1c489907b0664bf961cab11f8
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2016-08-18 14:34:15 +0900

    ok - make pwople who \have not stared at c macros as long as i have happy
    make it just {} instea dof do {} while (0) - as it seems that people
    dont get confused by macros having this but do get confused when its
    in code used as and indenter.

--- src/bin/e_askpass_main.c
+++ src/bin/e_askpass_main.c
@@ -33,7 +33,7 @@ cb_cancel(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info
 }
 
 EAPI int
-elm_main(int argc, char **argv)
+elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 {
    Evas_Object *win, *bx, *fr, *lab, *en, *sep, *bx2, *bt;
 
@@ -44,34 +44,34 @@ elm_main(int argc, char **argv)
    elm_app_compile_data_dir_set(PACKAGE_DATA_DIR);
    elm_app_info_set(elm_main, "enlightenment", "AUTHORS");
 
-   do {
+   {
       win = elm_win_util_standard_add("main", TITLE);
       elm_win_autodel_set(win, EINA_TRUE);
-      do {
+      {
          bx = elm_box_add(win);
          elm_box_horizontal_set(bx, EINA_FALSE);
          evas_object_size_hint_align_set(bx, EVAS_HINT_FILL, EVAS_HINT_FILL);
          elm_win_resize_object_add(win, bx);
-         do {
+         {
             fr = elm_frame_add(win);
             evas_object_size_hint_align_set(fr, EVAS_HINT_FILL, EVAS_HINT_FILL);
             elm_object_style_set(fr, PAD);
             elm_box_pack_end(bx, fr);
-            do {
+            {
                lab = elm_label_add(win);
                evas_object_size_hint_align_set(lab, EVAS_HINT_FILL, 0.5);
                elm_object_text_set(lab, TEXT);
                elm_object_content_set(fr, lab);
                evas_object_show(lab);
-            } while (0);
+            }
             evas_object_show(fr);
-         } while (0);
-         do {
+         }
+         {
             fr = elm_frame_add(win);
             evas_object_size_hint_align_set(fr, EVAS_HINT_FILL, EVAS_HINT_FILL);
             elm_object_style_set(fr, PAD);
             elm_box_pack_end(bx, fr);
-            do {
+            {
                en = elm_entry_add(win);
                entry = en;
                elm_entry_scrollable_set(en, EINA_TRUE);
@@ -86,53 +86,53 @@ elm_main(int argc, char **argv)
                elm_object_content_set(fr, en);
                evas_object_show(en);
                elm_object_focus_set(en, EINA_TRUE);
-            } while (0);
+            }
             evas_object_show(fr);
-         } while (0);
-         do {
+         }
+         {
             sep = elm_separator_add(win);
             elm_separator_horizontal_set(sep, EINA_TRUE);
             evas_object_size_hint_weight_set(sep, EVAS_HINT_EXPAND, 0.0);
             evas_object_size_hint_align_set(sep, EVAS_HINT_FILL, EVAS_HINT_FILL);
             elm_box_pack_end(bx, sep);
             evas_object_show(sep);
-         } while (0);
-         do {
+         }
+         {
             fr = elm_frame_add(win);
             evas_object_size_hint_align_set(fr, EVAS_HINT_FILL, EVAS_HINT_FILL);
             elm_object_style_set(fr, PAD);
             elm_box_pack_end(bx, fr);
-            do {
+            {
                bx2 = elm_box_add(win);
                elm_box_horizontal_set(bx2, EINA_TRUE);
                elm_box_homogeneous_set(bx2, EINA_TRUE);
                evas_object_size_hint_align_set(bx2, 0.5, 0.5);
                elm_object_content_set(fr, bx2);
-               do {
+               {
                   bt = elm_button_add(win);
                   elm_object_text_set(bt, OK);
                   evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
                   evas_object_smart_callback_add(bt, "clicked", cb_ok, NULL);
                   elm_box_pack_end(bx2, bt);
                   evas_object_show(bt);
-               } while (0);
-               do {
+               }
+               {
                   bt = elm_button_add(win);
                   elm_object_text_set(bt, CANCEL);
                   evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
                   evas_object_smart_callback_add(bt, "clicked", cb_cancel, NULL);
                   elm_box_pack_end(bx2, bt);
                   evas_object_show(bt);
-               } while (0);
+               }
                evas_object_show(bx2);
-            } while (0);
+            }
             evas_object_show(fr);
-         } while (0);
+         }
          evas_object_show(bx);
-      } while (0);
+      }
       elm_win_center(win, EINA_TRUE, EINA_TRUE);
       evas_object_show(win);
-   } while (0);
+   }
 
    elm_win_activate(win);
 
