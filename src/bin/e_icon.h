227d49e346941e86c584588420db94309b0dbe4d
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2016-12-31 10:04:51 +0900

    e icon header - clean up formatting

--- src/bin/e_icon.h
+++ src/bin/e_icon.h
@@ -7,14 +7,14 @@ EINTERN int e_icon_init(void);
 EINTERN int e_icon_shutdown(void);
 
 E_API Evas_Object *e_icon_add              (Evas *evas);
-E_API Eina_Bool    e_icon_file_set         (Evas_Object *obj, const char *file);
-E_API Eina_Bool    e_icon_file_key_set     (Evas_Object *obj, const char *file, const char *key);
-E_API Evas_Object *e_icon_edje_get(Evas_Object *obj);
-E_API Eina_Bool    e_icon_file_edje_set    (Evas_Object *obj, const char *file, const char *part);
-E_API Eina_Bool    e_icon_fdo_icon_set     (Evas_Object *obj, const char *icon);
-E_API void         e_icon_edje_object_set(Evas_Object *obj, Evas_Object *edje);
-E_API void         e_icon_image_object_set       (Evas_Object *obj, Evas_Object *o);
-E_API Eina_Bool   e_icon_file_get(const Evas_Object *obj, const char **file, const char **group);
+E_API void         e_icon_file_set         (Evas_Object *obj, const char *file);
+E_API void         e_icon_file_key_set     (Evas_Object *obj, const char *file, const char *key);
+E_API Evas_Object *e_icon_edje_get         (Evas_Object *obj);
+E_API void         e_icon_file_edje_set    (Evas_Object *obj, const char *file, const char *part);
+E_API void         e_icon_fdo_icon_set     (Evas_Object *obj, const char *icon);
+E_API void         e_icon_edje_object_set  (Evas_Object *obj, Evas_Object *edje);
+E_API void         e_icon_image_object_set (Evas_Object *obj, Evas_Object *o);
+E_API Eina_Bool    e_icon_file_get         (const Evas_Object *obj, const char **file, const char **group);
 E_API void         e_icon_smooth_scale_set (Evas_Object *obj, Eina_Bool smooth);
 E_API Eina_Bool    e_icon_smooth_scale_get (const Evas_Object *obj);
 E_API void         e_icon_alpha_set        (Evas_Object *obj, Eina_Bool smooth);
@@ -30,7 +30,7 @@ E_API void         e_icon_data_set         (Evas_Object *obj, void *data, int w,
 E_API void        *e_icon_data_get         (const Evas_Object *obj, int *w, int *h);
 E_API void         e_icon_scale_size_set   (Evas_Object *obj, int size);
 E_API int          e_icon_scale_size_get   (const Evas_Object *obj);
-E_API void         e_icon_selected_set	  (const Evas_Object *obj, Eina_Bool selected);
+E_API void         e_icon_selected_set     (const Evas_Object *obj, Eina_Bool selected);
 
 #endif
 #endif
