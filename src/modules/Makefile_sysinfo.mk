924307d96e357ba5b82e45216bf56ed3ddd1c331
Stephen 'Okra' Houston <smhouston88@gmail.com>
2017-01-06 16:16:13 -0600

    Enlightenment: Add the necessary Makefile for sysinfo.

---
+++ src/modules/Makefile_sysinfo.mk
@@ -0,0 +1,46 @@
+EXTRA_DIST += src/modules/sysinfo/module.desktop.in \
+src/modules/sysinfo/e-module-sysinfo.edj
+if USE_MODULE_SYSINFO
+sysinfodir = $(MDIR)/sysinfo
+sysinfo_DATA = src/modules/sysinfo/e-module-sysinfo.edj \
+	    src/modules/sysinfo/module.desktop
+
+
+sysinfopkgdir = $(MDIR)/sysinfo/$(MODULE_ARCH)
+sysinfopkg_LTLIBRARIES = src/modules/sysinfo/module.la
+
+src_modules_sysinfo_module_la_LIBADD = $(MOD_LIBS)
+src_modules_sysinfo_module_la_CPPFLAGS = $(MOD_CPPFLAGS) -Wall -Wextra -Wshadow
+src_modules_sysinfo_module_la_LDFLAGS = $(MOD_LDFLAGS)
+src_modules_sysinfo_module_la_SOURCES = src/modules/sysinfo/mod.c \
+			 src/modules/sysinfo/sysinfo.h \
+			 src/modules/sysinfo/batman/batman.h \
+                         src/modules/sysinfo/batman/batman.c \
+                         src/modules/sysinfo/batman/batman_fallback.c \
+                         src/modules/sysinfo/batman/batman_sysctl.c \
+                         src/modules/sysinfo/batman/batman_udev.c \
+                         src/modules/sysinfo/batman/batman_upower.c \
+                         src/modules/sysinfo/thermal/thermal.h \
+                         src/modules/sysinfo/thermal/thermal.c \
+                         src/modules/sysinfo/thermal/thermal_fallback.c \
+                         src/modules/sysinfo/thermal/thermal_sysctl.c \
+                         src/modules/sysinfo/thermal/thermal_udev.c \
+                         src/modules/sysinfo/cpuclock/cpuclock.h \
+                         src/modules/sysinfo/cpuclock/cpuclock.c \
+                         src/modules/sysinfo/cpuclock/cpuclock_sysfs.c \
+                         src/modules/sysinfo/cpuclock/cpuclock_sysctl.c \
+                         src/modules/sysinfo/cpumonitor/cpumonitor.h \
+                         src/modules/sysinfo/cpumonitor/cpumonitor.c \
+                         src/modules/sysinfo/cpumonitor/cpumonitor_proc.c \
+                         src/modules/sysinfo/memusage/memusage.h \
+                         src/modules/sysinfo/memusage/memusage.c \
+                         src/modules/sysinfo/memusage/memusage_proc.c \
+                         src/modules/sysinfo/netstatus/netstatus.h \
+                         src/modules/sysinfo/netstatus/netstatus.c \
+                         src/modules/sysinfo/netstatus/netstatus_proc.c \
+                         src/modules/sysinfo/sysinfo.c
+
+PHONIES += sysinfo install-sysinfo
+sysinfo: $(sysinfopkg_LTLIBRARIES) $(sysinfo_DATA)
+install-sysinfo: install-sysinfoDATA install-sysinfopkgLTLIBRARIES
+endif
