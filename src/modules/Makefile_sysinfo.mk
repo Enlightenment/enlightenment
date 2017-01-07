2a56bd46a5cb7481c5ec6cf84788875e7337098b
Stephen 'Okra' Houston <smhouston88@gmail.com>
2017-01-06 21:52:55 -0600

    Enlightenment: Make sure to compile sysinfo gadget for bsd correctly

--- src/modules/Makefile_sysinfo.mk
+++ src/modules/Makefile_sysinfo.mk
@@ -17,18 +17,12 @@ src_modules_sysinfo_module_la_SOURCES = src/modules/sysinfo/mod.c \
 			 src/modules/sysinfo/batman/batman.h \
                          src/modules/sysinfo/batman/batman.c \
                          src/modules/sysinfo/batman/batman_fallback.c \
-                         src/modules/sysinfo/batman/batman_sysctl.c \
-                         src/modules/sysinfo/batman/batman_udev.c \
-                         src/modules/sysinfo/batman/batman_upower.c \
                          src/modules/sysinfo/thermal/thermal.h \
                          src/modules/sysinfo/thermal/thermal.c \
                          src/modules/sysinfo/thermal/thermal_fallback.c \
-                         src/modules/sysinfo/thermal/thermal_sysctl.c \
-                         src/modules/sysinfo/thermal/thermal_udev.c \
                          src/modules/sysinfo/cpuclock/cpuclock.h \
                          src/modules/sysinfo/cpuclock/cpuclock.c \
                          src/modules/sysinfo/cpuclock/cpuclock_sysfs.c \
-                         src/modules/sysinfo/cpuclock/cpuclock_sysctl.c \
                          src/modules/sysinfo/cpumonitor/cpumonitor.h \
                          src/modules/sysinfo/cpumonitor/cpumonitor.c \
                          src/modules/sysinfo/cpumonitor/cpumonitor_proc.c \
@@ -39,6 +33,38 @@ src_modules_sysinfo_module_la_SOURCES = src/modules/sysinfo/mod.c \
                          src/modules/sysinfo/netstatus/netstatus.c \
                          src/modules/sysinfo/netstatus/netstatus_proc.c \
                          src/modules/sysinfo/sysinfo.c
+if HAVE_EEZE
+src_modules_sysinfo_module_la_SOURCES += src/modules/sysinfo/batman/batman_udev.c \
+                                         src/modules/sysinfo/thermal/thermal_udev.c
+else
+if HAVE_OPENBSD
+src_modules_sysinfo_module_la_SOURCES += src/modules/sysinfo/batman/batman_sysctl.c \
+                                         src/modules/sysinfo/thermal/thermal_sysctl.c \
+                                         src/modules/sysinfo/cpuclock/cpuclock_sysctl.c
+else
+if HAVE_NETBSD
+src_modules_sysinfo_module_la_SOURCES += src/modules/sysinfo/batman/batman_sysctl.c \
+                                         src/modules/sysinfo/thermal/thermal_sysctl.c \
+                                         src/modules/sysinfo/cpuclock/cpuclock_sysctl.c
+else
+if HAVE_DRAGONFLY
+src_modules_sysinfo_module_la_SOURCES += src/modules/sysinfo/batman/batman_sysctl.c \
+                                         src/modules/sysinfo/thermal/thermal_sysctl.c \
+                                         src/modules/sysinfo/cpuclock/cpuclock_sysctl.c
+else
+if HAVE_FREEBSD
+src_modules_sysinfo_module_la_SOURCES += src/modules/sysinfo/batman/batman_sysctl.c \
+                                         src/modules/sysinfo/thermal/thermal_sysctl.c \
+                                         src/modules/sysinfo/cpuclock/cpuclock_sysctl.c
+else
+src_modules_sysinfo_module_la_SOURCES += src/modules/sysinfo/batman/batman_upower.c \
+                                         src/modules/sysinfo/thermal/thermal_sysctl.c \
+                                         src/modules/sysinfo/cpuclock/cpuclock_sysctl.c
+endif
+endif
+endif
+endif
+endif
 
 PHONIES += sysinfo install-sysinfo
 sysinfo: $(sysinfopkg_LTLIBRARIES) $(sysinfo_DATA)
