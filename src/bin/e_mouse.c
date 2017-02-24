5b5e3fa5db13533e995dd29bcbe18781debf5bc4
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-02-24 14:15:42 -0500

    fix wl_drm detection in e_mouse.c

--- src/bin/e_mouse.c
+++ src/bin/e_mouse.c
@@ -1,10 +1,6 @@
 #include "e.h"
-#ifdef HAVE_WL_DRM
-# ifdef HAVE_DRM2
-#  include <Ecore_Drm2.h>
-# else
-#  include <Ecore_Drm2.h>
-# endif
+#ifdef USE_MODULE_WL_DRM
+# include <Ecore_Drm2.h>
 #endif
 
 E_API int
@@ -49,8 +45,7 @@ e_mouse_update(void)
      }
 #endif
 
-#ifdef HAVE_WL_DRM
-# ifdef HAVE_DRM2
+#ifdef USE_MODULE_WL_DRM
    if (strstr(ecore_evas_engine_name_get(e_comp->ee), "drm"))
      {
         Ecore_Drm2_Device *dev;
@@ -59,19 +54,6 @@ e_mouse_update(void)
         if (dev)
           ecore_drm2_device_pointer_left_handed_set(dev, (Eina_Bool)!e_config->mouse_hand);
      }
-# else
-   if (strstr(ecore_evas_engine_name_get(e_comp->ee), "drm"))
-     {
-        const Eina_List *list, *l;
-        Ecore_Drm_Device *dev;
-
-        list = ecore_drm_devices_get();
-        EINA_LIST_FOREACH(list, l, dev)
-          {
-             ecore_drm_device_pointer_left_handed_set(dev, (Eina_Bool)!e_config->mouse_hand);
-          }
-     }
-# endif
 #endif
    return 1;
 }
