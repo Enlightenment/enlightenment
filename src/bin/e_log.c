5d875e6a3dcd6a045610a6333cad9b6d15ebb2f1
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-02-24 14:15:42 -0500

    enable abort on critical error for non-release builds
    this should help with debugging

--- src/bin/e_log.c
+++ src/bin/e_log.c
@@ -39,6 +39,9 @@ e_log_init(void)
         if (level < 1)
           eina_log_print_cb_set(_e_log_cb, NULL);
      }
+#ifndef #E_RELEASE_BUILD
+   eina_log_abort_on_critical_set(1);
+#endif
    return e_log_dom != -1;
 }
 
