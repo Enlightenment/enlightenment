fe50e1edf0cadc608ebd23284c6aff2a95948de6
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2017-02-26 22:48:32 +0100

    tiling: do not pass parent as relative
    otherwise the client does not get into the list of parent

--- src/modules/tiling/window_tree.c
+++ src/modules/tiling/window_tree.c
@@ -124,7 +124,7 @@ tiling_window_tree_add(Window_Tree *root, Window_Tree *parent,
      {
         if (parent->children)
           {
-             _tiling_window_tree_parent_add(parent, new_node, parent, EINA_TRUE);
+             _tiling_window_tree_parent_add(parent, new_node, NULL, EINA_TRUE);
           }
         else
           {
