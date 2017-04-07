63be7d3b3f16f1284a0f06542b3982ad147b7570
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-04-07 16:07:25 -0400

    do not arbitrarily center "lost" child windows, center them on the parent
    if the parent is also lost, it will eventually be rescued and thus all children
    will also be saved
    
     #WeirdCommitLogs

--- src/bin/e_client.c
+++ src/bin/e_client.c
@@ -2238,7 +2238,12 @@ e_client_idler_before(void)
             )
           {
              if (e_config->screen_limits != E_CLIENT_OFFSCREEN_LIMIT_ALLOW_FULL)
-               _e_client_move_lost_window_to_center(ec);
+               {
+                  if (ec->parent)
+                    e_comp_object_util_center_on(ec->frame, ec->parent->frame);
+                  else
+                    _e_client_move_lost_window_to_center(ec);
+               }
           }
      }
 
