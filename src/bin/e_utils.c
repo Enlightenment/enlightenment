1b15a85d74d1de6bc3a8170f393e31043bdfd885
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-06-23 17:41:39 -0400

    print object type if name is null during shape debug

--- src/bin/e_utils.c
+++ src/bin/e_utils.c
@@ -1057,7 +1057,7 @@ e_util_size_debug(Evas_Object *obj)
 
    evas_object_geometry_get(obj, &x, &y, &w, &h);
    name = evas_object_name_get(obj);
-   fprintf(stderr, "%s %d OBJ[%s%s%p]: (%d,%d) - %dx%d\n", evas_object_visible_get(obj) ? "VIS" : "HID", evas_object_layer_get(obj), name ?: "", name ? "|" : "", obj, x, y, w, h);
+   fprintf(stderr, "%s %d OBJ[%s%s%p]: (%d,%d) - %dx%d\n", evas_object_visible_get(obj) ? "VIS" : "HID", evas_object_layer_get(obj), name ?: evas_object_type_get(obj), "|", obj, x, y, w, h);
 }
 
 static void
