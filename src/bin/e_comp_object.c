e184ffd49f6ace5a336a749315f42a5c9e9e251c
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-07-21 16:20:04 -0400

    do not use elm api to unset centered flag during resize
    this triggers a full eval which resizes the window and breaks the compositor

--- src/bin/e_comp_object.c
+++ src/bin/e_comp_object.c
@@ -1332,7 +1332,6 @@ _e_comp_intercept_resize(void *data, Evas_Object *obj, int w, int h)
        e_win_centered_get(cw->ec->internal_elm_win))
      {
         e_comp_object_util_center(obj);
-        elm_win_center(cw->ec->internal_elm_win, 0, 0);
      }
    cw->force_move = 0;
 }
