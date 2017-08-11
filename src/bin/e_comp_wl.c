31994d48765e6b6534c5a68f4bcd09f228018281
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-08-11 18:48:53 -0400

    don't center wl popups on their parents
    ref b817db7f09e6733fc59310a393494a60d9ea8910

--- src/bin/e_comp_wl.c
+++ src/bin/e_comp_wl.c
@@ -1349,7 +1349,7 @@ _e_comp_wl_surface_state_commit(E_Client *ec, E_Comp_Wl_Surface_State *state)
           {
              ec->take_focus = !starting || ec->internal_elm_win;
              ec->want_focus = ec->override && !e_client_util_is_popup(ec) && !ec->parent;
-             if (ec->parent && (!ec->lock_user_location))
+             if (ec->parent && (!ec->lock_user_location) && (!e_client_util_is_popup(ec)))
                e_comp_object_util_center_on(ec->frame, ec->parent->frame);
           }
      }
