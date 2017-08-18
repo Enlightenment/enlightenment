3aa8f2f1f74b536df83cd91e3babcccdfd70205b
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2017-08-18 09:13:52 +0900

    filepreview - properly set min size on cells not in a broken way
    this should fix T5891

--- src/bin/e_widget_filepreview.c
+++ src/bin/e_widget_filepreview.c
@@ -322,7 +322,14 @@ _e_wid_fprev_preview_video_widgets(E_Widget_Data *wd)
        e_widget_table_object_align_append(wd->o_preview_properties_table,         \
                                           wd->labob,                              \
                                           0, y, 1, 1, 0, 1, 0, 0, 1.0, 0.0);      \
-       o = e_widget_entry_add(win, &(wd->preview_extra_text), NULL, NULL, NULL); \
+       o = evas_object_rectangle_add(evas);                                       \
+       evas_object_size_hint_min_set(o, entw * e_scale, 1);                       \
+       evas_object_color_set(o, 0, 0, 0, 0);                                      \
+       evas_object_pass_events_set(o, EINA_TRUE);                                 \
+       e_widget_table_object_align_append(wd->o_preview_properties_table,         \
+                                          o,                                      \
+                                          1, y, 1, 1, 1, 1, 1, 0, 0.0, 0.0);      \
+       o = e_widget_entry_add(win, &(wd->preview_extra_text), NULL, NULL, NULL);  \
        e_widget_entry_readonly_set(o, 1);                                         \
        wd->entob = o;                                                             \
        e_widget_table_object_align_append(wd->o_preview_properties_table,         \
@@ -405,9 +412,14 @@ _e_wid_fprev_preview_fs_widgets(E_Widget_Data *wd, Eina_Bool mount_point)
        e_widget_table_object_align_append(wd->o_preview_properties_table,         \
                                           wd->labob,                              \
                                           0, y, 1, 1, 0, 1, 0, 0, 1.0, 0.0);      \
-       o = e_widget_entry_add(win, &(wd->preview_extra_text), NULL, NULL, NULL); \
-       evas_object_size_hint_min_get(o, &mw, &mh); \
-       e_widget_size_min_set(o, 100, mh); \
+       o = evas_object_rectangle_add(evas);                                       \
+       evas_object_size_hint_min_set(o, entw * e_scale, 1);                       \
+       evas_object_color_set(o, 0, 0, 0, 0);                                      \
+       evas_object_pass_events_set(o, EINA_TRUE);                                 \
+       e_widget_table_object_align_append(wd->o_preview_properties_table,         \
+                                          o,                                      \
+                                          1, y, 1, 1, 1, 1, 1, 0, 0.0, 0.0);      \
+       o = e_widget_entry_add(win, &(wd->preview_extra_text), NULL, NULL, NULL);  \
        e_widget_entry_readonly_set(o, 1);                                         \
        wd->entob = o;                                                             \
        e_widget_table_object_align_append(wd->o_preview_properties_table,         \
