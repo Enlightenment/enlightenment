1c9f9bd32cdc2e64e83e9e1a154faf25ea46906c
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-08-18 15:52:52 -0400

    use menu item geometry to determine whether menu item is onscreen
    fix T2126

--- src/modules/conf_theme/e_int_config_color_classes.c
+++ src/modules/conf_theme/e_int_config_color_classes.c
@@ -32,7 +32,7 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas EINA_UNUSED, E_Config_Dia
 
    e_dialog_resizable_set(cfd->dia, 1);
    o = elm_color_class_editor_add(cfd->dia->win);
-   evas_object_size_hint_min_set(o, 400, 280);
+   elm_win_resize_object_add(cfd->dia->win, o);
 
    e_util_win_auto_resize_fill(cfd->dia->win);
    elm_win_center(cfd->dia->win, 1, 1);
