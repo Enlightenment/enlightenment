1af5125cc93e153049f5e3c598fc09187f4152d2
Stephen 'Okra' Houston <smhouston88@gmail.com>
2017-08-24 08:08:41 -0500

    Batman: Set the demo gadget to have full battery.

--- src/modules/sysinfo/batman/batman.c
+++ src/modules/sysinfo/batman/batman.c
@@ -392,7 +392,7 @@ _batman_config_updated(Instance *inst)
 
    if (inst->cfg->id == -1)
      {
-        _batman_face_level_set(inst->cfg->batman.o_gadget, .60);
+        _batman_face_level_set(inst->cfg->batman.o_gadget, 1.0);
         return;
      }
    if ((inst->cfg->batman.force_mode == UNKNOWN) ||
