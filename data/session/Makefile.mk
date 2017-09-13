0921b8859a1a0175f311250519351538930792df
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-09-13 12:00:56 -0400

    install session files conditionally based on the available session types

--- data/session/Makefile.mk
+++ data/session/Makefile.mk
@@ -1,5 +1,11 @@
+if ! HAVE_WAYLAND_ONLY
 xsessionfilesdir = $(datadir)/xsessions
 xsessionfiles_DATA = data/session/enlightenment.desktop
+endif
+
+if HAVE_WAYLAND
 wlsessionfilesdir = $(datadir)/wayland-sessions
 wlsessionfiles_DATA = data/session/enlightenment.desktop
+endif
+
 EXTRA_DIST += data/session/enlightenment.desktop.in
