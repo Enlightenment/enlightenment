42e720a7edf0828f88d4d739382d4f2fea672bac
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-09-21 13:31:04 -0400

    don't perform zone updates on client resize
    zone updates should probably only happen on client moves, not resizes
    
    ref T6023

--- src/bin/e_client.c
+++ src/bin/e_client.c
@@ -1453,7 +1453,6 @@ _e_client_cb_evas_resize(void *data, Evas *e EINA_UNUSED, Evas_Object *obj EINA_
 
    _e_client_event_simple(ec, E_EVENT_CLIENT_RESIZE);
 
-   if (!ec->ignored) _e_client_zone_update(ec);
    evas_object_geometry_get(ec->frame, &x, &y, &w, &h);
    if ((e_config->transient.resize) && (ec->transients))
      {
