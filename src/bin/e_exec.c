2082bb51d3abffd991b4d791ace2567888e2e9fb
Stephen 'Okra' Houston <smhouston88@gmail.com>
2017-09-26 22:01:15 -0500

    E Exe: Since we no longer ref clients in the phony cb, don't free them after reffing them in instance_client_add.

--- src/bin/e_exec.c
+++ src/bin/e_exec.c
@@ -361,7 +361,6 @@ e_exec_instance_client_add(E_Exec_Instance *inst, E_Client *ec)
    e_object_ref(E_OBJECT(ec));
    ec->exe_inst = inst;
    inst->ref++;
-   ecore_event_add(E_EVENT_EXEC_NEW_CLIENT, inst, _e_exec_cb_exec_new_client_free, ec);
 }
 
 E_API void
