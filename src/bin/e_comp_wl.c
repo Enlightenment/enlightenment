42d707909e74a3cb5a901e210747c5daa06417b2
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-10-06 11:37:50 -0400

    simplify focus on commit block for wl clients
    none of this applies to popups

--- src/bin/e_comp_wl.c
+++ src/bin/e_comp_wl.c
@@ -1347,13 +1347,13 @@ _e_comp_wl_surface_state_commit(E_Client *ec, E_Comp_Wl_Surface_State *state)
    if (state->new_attach)
      {
         _e_comp_wl_surface_state_attach(ec, state);
-        if (first && (!ec->comp_data->cursor))
+        if (first && (!ec->comp_data->cursor) && (!e_client_util_is_popup(ec)))
           {
              if (!e_client_has_xwindow(ec))
                {
-                  ec->take_focus = !e_client_util_is_popup(ec) && (!starting || ec->internal_elm_win);
-                  ec->want_focus = ec->override && !e_client_util_is_popup(ec) && !ec->parent;
-                  if (ec->parent && (!ec->lock_user_location) && (!e_client_util_is_popup(ec)))
+                  ec->take_focus = !starting || ec->internal_elm_win;
+                  ec->want_focus = ec->override && !ec->parent;
+                  if (ec->parent && (!ec->lock_user_location))
                     e_comp_object_util_center_on(ec->frame, ec->parent->frame);
                }
           }
