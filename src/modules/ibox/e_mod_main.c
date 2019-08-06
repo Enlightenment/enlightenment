bb390716f54fd6d34f52364d88a4c1357a852e72
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2019-08-06 12:40:08 +0100

    ibox - make coverity happy by reordering free then list remove
    fix CID 1403955

--- src/modules/ibox/e_mod_main.c
+++ src/modules/ibox/e_mod_main.c
@@ -1130,8 +1130,8 @@ _ibox_cb_event_client_uniconify(void *data EINA_UNUSED, int type EINA_UNUSED, vo
         int mw, mh, h;
         ic = _ibox_icon_find(b, ev->ec);
         if (!ic) continue;
-        _ibox_icon_free(ic);
         b->icons = eina_list_remove(b->icons, ic);
+        _ibox_icon_free(ic);
         _ibox_empty_handle(b);
         _ibox_resize_handle(b);
         _gc_orient(b->inst->gcc, -1);
