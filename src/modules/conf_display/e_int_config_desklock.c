a3d981fa7457310fef81bd0f559e3f74d6314d60
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2020-07-15 01:32:00 +0100

    desklock config - remove infinite loop - bug added in gad remove
    accidentally added a widget again to the list thus causing a loop.
    didnt delete the append when removing in gad remove commit

--- src/modules/conf_display/e_int_config_desklock.c
+++ src/modules/conf_display/e_int_config_desklock.c
@@ -270,7 +270,6 @@ _basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
    e_widget_disabled_set(cfdata->lock_cmd_entry,
      (cfdata->desklock_auth_method != E_DESKLOCK_AUTH_METHOD_EXTERNAL));
 
-   e_widget_list_object_append(ol, ow, 1, 1, 0.5);
    e_widget_toolbook_page_append(otb, NULL, _("Locking"), ol,
                                  1, 1, 1, 0, 0.0, 0.0);
 
