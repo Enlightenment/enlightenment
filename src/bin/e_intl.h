595babc0eb7eaff2d444d741000ae9811121d9c8
Christopher Michael <devilhorns@comcast.net>
2020-09-17 12:14:20 -0400

    e_intl: Fix formatting
    NB: No functional changes

--- src/bin/e_intl.h
+++ src/bin/e_intl.h
@@ -3,33 +3,33 @@
 #  if E_INTERNAL
 
 #   ifdef HAVE_GETTEXT
-#define _(str) gettext(str)
-#define d_(str, dom) dgettext(PACKAGE dom, str)
-#define P_(str, str_p, n) ngettext(str, str_p, n)
-#define dP_(str, str_p, n, dom) dngettext(PACKAGE dom, str, str_p, n)
+#    define _(str) gettext(str)
+#    define d_(str, dom) dgettext(PACKAGE dom, str)
+#    define P_(str, str_p, n) ngettext(str, str_p, n)
+#    define dP_(str, str_p, n, dom) dngettext(PACKAGE dom, str, str_p, n)
 #   else
-#define _(str) (str)
-#define d_(str, dom) (str)
-#define P_(str, str_p, n) (str_p)
-#define dP_(str, str_p, n, dom) (str_p)
+#    define _(str) (str)
+#    define d_(str, dom) (str)
+#    define P_(str, str_p, n) (str_p)
+#    define dP_(str, str_p, n, dom) (str_p)
 #   endif
 /* These macros are used to just mark strings for translation, this is useful
  * for string lists which are not dynamically allocated
  */
-#define N_(str) (str)
-#define NP_(str, str_p) str, str_p
+#   define N_(str) (str)
+#   define NP_(str, str_p) str, str_p
 #  endif
 # endif
 typedef struct _E_Locale_Parts E_Locale_Parts;
 
-# else
-#ifndef E_INTL_H
-#define E_INTL_H
+#else
+# ifndef E_INTL_H
+#  define E_INTL_H
 
-#define E_INTL_LOC_CODESET   1 << 0
-#define E_INTL_LOC_REGION    1 << 1
-#define E_INTL_LOC_MODIFIER  1 << 2
-#define E_INTL_LOC_LANG      1 << 3
+#  define E_INTL_LOC_CODESET   1 << 0
+#  define E_INTL_LOC_REGION    1 << 1
+#  define E_INTL_LOC_MODIFIER  1 << 2
+#  define E_INTL_LOC_LANG      1 << 3
 
 struct _E_Locale_Parts
 {
