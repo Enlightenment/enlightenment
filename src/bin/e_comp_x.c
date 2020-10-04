f1a15a41db619fca3fbcae6b7f224b2d5ce3ef44
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2020-10-04 22:11:13 +0100

    blanking - workaround unknown issue where ss resets to 1sec after resume
    trying workaround to fix T8793
    @fix

--- src/bin/e_comp_x.c
+++ src/bin/e_comp_x.c
@@ -5199,6 +5199,7 @@ _e_comp_cb_pointer_suspend_resume_done(void *data, Evas_Object *obj, const char 
         _e_comp_pointer_ungrab();
         e_pointer_grab_set(e_comp->pointer, EINA_FALSE);
      }
+   e_screensaver_update();
 }
 
 EINTERN Eina_Bool
@@ -5241,6 +5242,7 @@ _e_comp_x_screensaver_off()
 {
    const char *s;
 
+   e_screensaver_update();
    _e_comp_pointer_ungrab();
    e_pointer_grab_set(e_comp->pointer, EINA_FALSE);
    if ((!e_comp->pointer) || (!e_comp->pointer->o_ptr)) return ECORE_CALLBACK_RENEW;
