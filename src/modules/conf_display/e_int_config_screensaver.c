6654560118fb3512380b87f48f0de8c1f846ea8e
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2020-10-15 18:42:38 +0100

    presentation - remove final bits in screen blanking settings

--- src/modules/conf_display/e_int_config_screensaver.c
+++ src/modules/conf_display/e_int_config_screensaver.c
@@ -13,7 +13,6 @@ static void _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 static int _basic_apply(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 static int _basic_check_changed(E_Config_Dialog *cfd EINA_UNUSED, E_Config_Dialog_Data *cfdata);
 static Evas_Object *_basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata);
-static void _cb_ask_presentation_changed(void *data, Evas_Object *obj);
 
 struct _E_Config_Dialog_Data
 {
@@ -22,8 +21,6 @@ struct _E_Config_Dialog_Data
    int enable_screensaver;
    double timeout;
    double desklock_timeout;
-   int ask_presentation;
-   double ask_presentation_timeout;
 
    int screensaver_suspend;
    int screensaver_hibernate;
@@ -37,11 +34,9 @@ struct _E_Config_Dialog_Data
    int no_dpms_on_fullscreen;
    int use_dpms;
 
-   struct
-     {
-        Evas_Object *ask_presentation_slider;
-        Evas_Object *slider_suspend_seconds;
-     } gui;
+   struct {
+      Evas_Object *slider_suspend_seconds;
+   } gui;
 };
 
 E_Config_Dialog *
@@ -73,8 +68,6 @@ _fill_data(E_Config_Dialog_Data *cfdata)
    cfdata->enable_screensaver = e_config->screensaver_enable;
    cfdata->timeout = (double)e_config->screensaver_timeout / 60.0;
    cfdata->desklock_timeout = (double)e_config->screensaver_desklock_timeout;
-   cfdata->ask_presentation = e_config->screensaver_ask_presentation;
-   cfdata->ask_presentation_timeout = e_config->screensaver_ask_presentation_timeout;
    cfdata->screensaver_suspend = e_config->screensaver_suspend;
    cfdata->screensaver_hibernate = e_config->screensaver_hibernate;
    cfdata->screensaver_suspend_on_ac = e_config->screensaver_suspend_on_ac;
@@ -111,8 +104,6 @@ _basic_apply(E_Config_Dialog *cfd EINA_UNUSED, E_Config_Dialog_Data *cfdata)
    e_config->screensaver_enable = cfdata->enable_screensaver;
    e_config->screensaver_timeout = lround(cfdata->timeout * 60.0);
    e_config->screensaver_desklock_timeout = lround(cfdata->desklock_timeout);
-   e_config->screensaver_ask_presentation = cfdata->ask_presentation;
-   e_config->screensaver_ask_presentation_timeout = cfdata->ask_presentation_timeout;
    e_config->screensaver_suspend = cfdata->screensaver_suspend;
    e_config->screensaver_hibernate = cfdata->screensaver_hibernate;
    e_config->screensaver_suspend_on_ac = cfdata->screensaver_suspend_on_ac;
@@ -173,8 +164,6 @@ _basic_check_changed(E_Config_Dialog *cfd EINA_UNUSED, E_Config_Dialog_Data *cfd
    return ((e_config->screensaver_enable != cfdata->enable_screensaver) ||
           (e_config->screensaver_timeout != lround(cfdata->timeout * 60.0)) ||
           (e_config->screensaver_desklock_timeout != lround(cfdata->desklock_timeout)) ||
-          (e_config->screensaver_ask_presentation != cfdata->ask_presentation) ||
-          (!EINA_DBL_EQ(e_config->screensaver_ask_presentation_timeout, cfdata->ask_presentation_timeout)) ||
           (e_config->screensaver_suspend != cfdata->screensaver_suspend) ||
           (e_config->screensaver_hibernate != cfdata->screensaver_hibernate) ||
           (e_config->screensaver_suspend_on_ac != cfdata->screensaver_suspend_on_ac) ||
@@ -267,34 +256,7 @@ _basic_create(E_Config_Dialog *cfd EINA_UNUSED, Evas *evas, E_Config_Dialog_Data
    e_widget_list_object_append(ol, oc, 1, 1, 0.5);
    e_widget_toolbook_page_append(otb, NULL, _("Wakeups"), ol,
                                  1, 1, 1, 0, 0.0, 0.0);
-   /* Presentation */
-   ol = e_widget_list_add(evas, 0, 0);
-   oc2 = e_widget_check_add(evas, _("Suggest if deactivated before"),
-                           &(cfdata->ask_presentation));
-   e_widget_check_widget_disable_on_unchecked_add(oc, oc2);
-   e_widget_list_object_append(ol, oc2, 1, 1, 0.5);
-   ow = e_widget_slider_add(evas, 1, 0, _("%1.0f seconds"),
-			    1.0, 300.0, 10.0, 0,
-			    &(cfdata->ask_presentation_timeout), NULL, 100);
-   cfdata->gui.ask_presentation_slider = ow;
-   e_widget_check_widget_disable_on_unchecked_add(oc, ow);
-   e_widget_check_widget_disable_on_unchecked_add(oc2, ow);
-   e_widget_on_disable_hook_set(ow, _cb_ask_presentation_changed, cfdata);
-   e_widget_list_object_append(ol, ow, 1, 1, 0.5);
-   e_widget_toolbook_page_append(otb, NULL, _("Presentation"), ol,
-                                 1, 1, 1, 0, 0.0, 0.0);
-
    e_widget_toolbook_page_show(otb, 0);
 
    return otb;
-}
-
-static void
-_cb_ask_presentation_changed(void *data, Evas_Object *obj EINA_UNUSED)
-{
-   E_Config_Dialog_Data *cfdata = data;
-   Eina_Bool disable;
-
-   disable = ((!cfdata->enable_screensaver) || (!cfdata->ask_presentation));
-   e_widget_disabled_set(cfdata->gui.ask_presentation_slider, disable);
 }
