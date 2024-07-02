b2b41edc0151ed2d9f7a99bd5ca83c1df3c3cfc4
Thanatermesis <thanatermesis@gmail.com>
2024-07-02 03:21:41 -0500

    tell when the screen option is in interlaced mode

--- src/modules/conf_randr/e_int_config_randr2.c
+++ src/modules/conf_randr/e_int_config_randr2.c
@@ -414,6 +414,8 @@ _basic_screen_info_fill(E_Config_Dialog_Data *cfdata, E_Config_Randr2_Screen *cs
              mode_cbdata->mode = *m;
              if (m->flags & ECORE_X_RANDR_MODE_DOUBLE_SCAN)
                snprintf(buf, sizeof(buf), "%ix%i @ %1.2fHz (Doublescan)", m->w, m->h, m->refresh / 2);
+             else if (m->flags & ECORE_X_RANDR_MODE_INTERLACE)
+               snprintf(buf, sizeof(buf), "%ix%i @ %1.2fHz (Interlaced)", m->w, m->h, m->refresh);
              else
                snprintf(buf, sizeof(buf), "%ix%i @ %1.2fHz", m->w, m->h, m->refresh);
              it = elm_list_item_append(cfdata->modes_obj, buf, NULL, NULL, _cb_mode_set, mode_cbdata);
