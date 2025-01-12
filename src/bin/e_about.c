c9f1bfa8118e3f613a885f30af7e6be557a85d28
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2025-01-12 00:27:09 +0000

    we're in 29025 now

--- src/bin/e_about.c
+++ src/bin/e_about.c
@@ -23,7 +23,7 @@ e_about_new(void)
    snprintf
      (buf, sizeof(buf), "%s%s",
      _(
-       "<title>Copyright &copy; 2000-2024, by the Enlightenment "
+       "<title>Copyright &copy; 2000-2025, by the Enlightenment "
        "Development Team</><ps/>"
        "<ps/>"
        "We hope you enjoy using this software as much as we enjoyed "
