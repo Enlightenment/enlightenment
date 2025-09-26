a5b841d308443f7c87b66b3e3a4b852f561b42ff
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2025-09-26 21:26:41 +0100

    suspent smart - fix deferred suspend to work if more pwsv events happen
    @fix

--- src/bin/e_powersave.c
+++ src/bin/e_powersave.c
@@ -376,9 +376,9 @@ _e_powersave_event_update_free(void *data EINA_UNUSED, void *event)
           e_sys_action_do(E_SYS_HIBERNATE, NULL);
         else if (powersave_deferred_suspend)
           e_sys_action_do(E_SYS_SUSPEND_MODE, NULL);
+        powersave_deferred_hibernate = EINA_FALSE;
+        powersave_deferred_suspend = EINA_FALSE;
      }
-   powersave_deferred_hibernate = EINA_FALSE;
-   powersave_deferred_suspend = EINA_FALSE;
 }
 
 static void
